cf5fce3cfd902161510e5fd1619b004851bd9070
Nicholas Marques <nnmarques97@gmail.com>
2022-02-02 16:20:22 -0300

    feat: add #impact_for to NonProfit

--- app/models/non_profit.rb
+++ app/models/non_profit.rb
@@ -6,4 +6,8 @@ class NonProfit < ApplicationRecord
   has_many :non_profit_impacts
 
   validates :name, :impact_description, :wallet_address, presence: true
+
+  def impact_for(date: Time.zone.now)
+    non_profit_impacts.find_by('start_date <= ? AND end_date >= ?', date, date)
+  end
 end

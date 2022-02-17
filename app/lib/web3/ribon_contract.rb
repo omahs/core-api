82ce66012b0e79a667ee12fd566078edddea4bf5
Clara Mendes <claraplanejamento@gmail.com>
2022-02-17 10:46:50 -0300

    fix: removes unused variable

--- app/lib/web3/ribon_contract.rb
+++ app/lib/web3/ribon_contract.rb
@@ -1,11 +1,11 @@
 module Web3
   class RibonContract
     def self.donate_through_integration(non_profit:, user:, amount:)
-      results = HTTParty.post('https://tsmzfesl49.execute-api.us-east-1.amazonaws.com/dev',
-                               { body:
-                                 { nonProfit: non_profit,
-                                   user: user,
-                                   amount: amount }.to_json })
+      HTTParty.post('https://tsmzfesl49.execute-api.us-east-1.amazonaws.com/dev',
+                    { body:
+                      { nonProfit: non_profit,
+                        user: user,
+                        amount: amount }.to_json })
     end
   end
 end

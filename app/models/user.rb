c27a4e9ee507f0bb49c868d80d6a66133dec531b
Nicholas Marques <nnmarques97@gmail.com>
2022-03-09 13:39:32 -0300

    feat: add set_user_donation_stats to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
 
   before_validation { email.downcase! }
   has_many :donations
+  has_one :user_donation_stats
+
+  after_create :set_user_donation_stats
+
+  delegate :last_donation_at, to: :user_donation_stats
+
+  private
+
+  def set_user_donation_stats
+    create_user_donation_stats unless user_donation_stats
+  end
 end

2ec9a1570f044fb843c05dd875d287044253b244
Nicholas Marques <nnmarques97@gmail.com>
2022-03-23 11:44:08 -0300

    test: add user_impacts controller spec

--- spec/requests/api/v1/users/impacts_spec.rb
+++ spec/requests/api/v1/users/impacts_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Users::Impacts', type: :request do
+  subject(:request) { get "/api/v1/users/#{user.id}/impacts" }
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    let(:user) { create(:user) }
+    let(:non_profit) { build(:non_profit) }
+
+    before do
+      allow(User).to receive(:find).and_return(user)
+      allow(user).to receive(:impact).and_return([{
+                                                   non_profit: non_profit,
+                                                   impact: 5
+                                                 }])
+    end
+
+    it 'returns the user impact by ngo' do
+      request
+
+      expect(response_json.first.keys).to match_array %w[impact non_profit]
+    end
   end
 end

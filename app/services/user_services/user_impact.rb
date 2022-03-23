87f8adf59069bf8ea18982fcc62cb96fc5344658
Nicholas Marques <nnmarques97@gmail.com>
2022-03-23 10:27:16 -0300

    feat: add user_impact service and graphql factories

--- app/services/user_services/user_impact.rb
+++ app/services/user_services/user_impact.rb
@@ -4,7 +4,9 @@ module UserServices
       donation_balances = Graphql::RibonApi::Client.query(Graphql::Queries::FetchDonationBalances::Query)
       user_donations = select_from_donation_balances(donation_balances)
 
-      format_user_donations(user_donations)
+      NonProfit.all.map do |non_profit|
+        { non_profit: non_profit, impact: impact_sum_by_non_profit(user_donations, non_profit) }
+      end
     end
 
     private
@@ -15,21 +17,24 @@ module UserServices
       end
     end
 
-    def format_user_donations(user_donations)
-      user_donations.map do |donation|
-        {
-          non_profit: non_profit_from_address(donation['nonProfit']),
-          total_donated: donation['totalDonated']
-        }
-      end
-    end
-
     def non_profit_from_address(wallet_address)
       NonProfit.find_by('lower(wallet_address) = ?', wallet_address.downcase)
     end
 
     def hashed_email
       "0x#{Digest::Keccak.new(256).hexdigest(email)}"
+    end
+
+    def impact_sum_by_non_profit(user_donations, non_profit)
+      non_profit_donations = user_donations
+                             .select do |donation|
+        donation['nonProfit']
+          .casecmp(non_profit.wallet_address).zero?
+      end
+      total_usd_cents_donated = non_profit_donations
+                                .sum { |donation| donation['totalDonated'].to_i }
+
+      total_usd_cents_donated / non_profit.impact_for(date: Time.zone.now).usd_cents_to_one_impact_unit
     end
   end
 end
---
+++ spec/services/user_services/user_impact_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe UserServices::UserImpact, type: :service do
+  describe '#impact' do
+    let(:user) { build(:user, email: 'user@test.com') }
+    let(:non_profit1) do
+      create(:non_profit, :with_impact,
+             wallet_address: '0xf20c382d2a95eb19f9164435aed59e5c59bc1fd9')
+    end
+    let(:non_profit2) do
+      create(:non_profit, :with_impact,
+             wallet_address: '0xg20c382d2a95eb19f9164435aed59e5c59bc1fd9')
+    end
+
+    before do
+      non_profit1
+      non_profit2
+      allow(Graphql::RibonApi::Client).to receive(:query).and_return(build(:fetch_donation_balances_query))
+    end
+
+    it 'returns the sum of impact' do
+      expect(user.impact.first[:impact]).to eq 180_000_000_000_000
+      expect(user.impact.last[:impact]).to eq 10_000_000_000_000
+    end
+
+    it 'returns the non profits' do
+      expect(user.impact.first[:non_profit]).to eq non_profit1
+      expect(user.impact.last[:non_profit]).to eq non_profit2
+    end
+  end
+end

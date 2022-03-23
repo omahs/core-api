dcdb69369b7d02b8571a7f6b78791462506cf50d
Nicholas Marques <nnmarques97@gmail.com>
2022-03-23 08:42:21 -0300

    refactor: update UserServices user_impact methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include UserServices::UserImpact
+
   validates :email, uniqueness: { case_sensitive: true }, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   before_validation { email.downcase! }
@@ -10,29 +12,6 @@ class User < ApplicationRecord
 
   delegate :last_donation_at, to: :user_donation_stats
   delegate :can_donate?, to: :user_donation_stats
-
-  def user_impact
-    donation_balances = Graphql::RibonApi::Client.query(Graphql::Queries::FetchDonationBalances::Query)
-
-    user_donations = donation_balances.original_hash['data']['donationBalances'].select do |donation|
-      donation['user'] == hashed_email
-    end
-
-    result = []
-
-    user_donations.each do |donation|
-      result << {
-        non_profit: donation['nonProfit'],
-        total_donated: donation['totalDonated']
-      }
-    end
-
-    result
-  end
-
-  def hashed_email
-    "0x#{Digest::Keccak.new(256).hexdigest(email)}"
-  end
 
   private
 
---
+++ app/services/user_services/user_impact.rb
@@ -0,0 +1,35 @@
+module UserServices
+  module UserImpact
+    def impact
+      donation_balances = Graphql::RibonApi::Client.query(Graphql::Queries::FetchDonationBalances::Query)
+      user_donations = select_from_donation_balances(donation_balances)
+
+      format_user_donations(user_donations)
+    end
+
+    private
+
+    def non_profit_from_address(wallet_address)
+      NonProfit.find_by('lower(wallet_address) = ?', wallet_address.downcase)
+    end
+
+    def select_from_donation_balances(donation_balances)
+      donation_balances.original_hash['data']['donationBalances'].select do |donation|
+        donation['user'] == hashed_email
+      end
+    end
+
+    def format_user_donations(user_donations)
+      user_donations.map do |donation|
+        {
+          non_profit: non_profit_from_address(donation['nonProfit']),
+          total_donated: donation['totalDonated']
+        }
+      end
+    end
+
+    def hashed_email
+      "0x#{Digest::Keccak.new(256).hexdigest(email)}"
+    end
+  end
+end

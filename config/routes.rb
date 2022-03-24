e949616e53284eaa62c43a43fac65496529783a4
Nicholas Marques <nnmarques97@gmail.com>
2022-03-24 09:03:03 -0300

    fix: remove graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   Rails.application.routes.draw do
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
     post "/graphql", to: "graphql#execute"
   end
 

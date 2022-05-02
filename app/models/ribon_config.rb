12140bca30cdd2b2a992714020f9bd3ec6fc60a1
Nicholas Marques <nnmarques97@gmail.com>
2022-05-02 15:24:46 -0300

    feat: add stop destroy to RibonConfig

--- app/models/ribon_config.rb
+++ app/models/ribon_config.rb
@@ -1,6 +1,8 @@
 class RibonConfig < ApplicationRecord
-  validate :singularity
+  validate :singularity, on: :create
   validates :default_ticket_value, presence: true
+
+  before_destroy :stop_destroy
 
   def self.default_ticket_value
     first.default_ticket_value
@@ -12,5 +14,10 @@ class RibonConfig < ApplicationRecord
     raise StandardError, 'There can be only one.' if RibonConfig.count.positive?
   rescue StandardError => e
     errors.add(:message, e.message)
+  end
+
+  def stop_destroy
+    errors.add(:base, :undestroyable)
+    throw :abort
   end
 end

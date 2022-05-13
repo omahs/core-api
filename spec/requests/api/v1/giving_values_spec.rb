eb2700a6f38ccdcaf30c0ed172144688d08d8e92
Nicholas Marques <nnmarques97@gmail.com>
2022-05-13 11:54:36 -0300

    refactor: update giving values name

--- spec/requests/api/v1/giving_values_spec.rb
+++ spec/requests/api/v1/giving_values_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Api::V1::GivingValues', type: :request do
       create_list(:giving_value, 2, currency: :usd)
     end
 
-    it 'returns a list of non profits' do
+    it 'returns a list of giving values' do
       request
 
       expect_response_collection_to_have_keys(%w[created_at id updated_at value value_text currency])

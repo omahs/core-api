e0a8ce3298defbbfc16255433329863bc762c1e7
Nicholas Marques <nnmarques97@gmail.com>
2022-05-13 11:49:43 -0300

    test: add validations tests to giving value

--- spec/models/giving_value_spec.rb
+++ spec/models/giving_value_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe GivingValue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.validations' do
+    subject { build(:giving_value) }
+
+    it { is_expected.to validate_presence_of(:value) }
+    it { is_expected.to validate_presence_of(:currency) }
+  end
+
+  describe "#currency_symbol" do
+    context "when it is usd" do
+      it 'returns $' do
+        giving_value = build(:giving_value, currency: :usd)
+
+        expect(giving_value.currency_symbol).to eq "$"
+      end
+    end
+
+    context "when it is brl" do
+      it 'returns R$' do
+        giving_value = build(:giving_value, currency: :brl)
+
+        expect(giving_value.currency_symbol).to eq "R$"
+      end
+    end
+  end
 end

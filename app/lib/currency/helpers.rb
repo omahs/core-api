c77b3894c6e8151d7d7a8278655ccee32a179fc1
Nicholas Marques <nnmarques97@gmail.com>
2022-05-26 09:05:12 -0300

    test: add tests to currency helpers

--- app/lib/currency/helpers.rb
+++ app/lib/currency/helpers.rb
@@ -4,21 +4,15 @@ module Currency
 
     CURRENCIES.each do |currency|
       define_method :"convert_to_#{currency}" do |args|
-        new(from: args[:from], value: args[:value], to: currency_string(currency)).convert
+        new(from: args[:from], value: args[:value], to: currency).convert
       end
       CURRENCIES.each do |currency_from|
         next if currency.eql?(currency_from)
 
         define_method :"convert_from_#{currency_from}_to_#{currency}" do |value|
-          new(from: currency_string(currency_from), value: value, to: currency_string(currency)).convert
+          new(from: currency_from, value: value, to: currency).convert
         end
       end
-    end
-
-    private
-
-    def currency_string(currency)
-      currency.to_s.upcase
     end
   end
 end

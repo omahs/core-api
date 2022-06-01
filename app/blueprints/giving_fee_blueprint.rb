b8ea75bf908b964c0aeb6860b46a3e5dcbeee776
Nicholas Marques <nnmarques97@gmail.com>
2022-06-01 08:18:09 -0300

    refactor: update GivingFee blueprint fields

--- app/blueprints/giving_fee_blueprint.rb
+++ app/blueprints/giving_fee_blueprint.rb
@@ -1,3 +1,3 @@
 class GivingFeeBlueprint < Blueprinter::Base
-  fields %i[card_fee crypto_fee crypto_giving giving_total net_giving service_fees]
+  fields :card_fee, :crypto_fee, :crypto_giving, :giving_total, :net_giving, :service_fees
 end
--- app/controllers/api/v1/givings/fees_controller.rb
+++ app/controllers/api/v1/givings/fees_controller.rb
@@ -3,14 +3,24 @@ module Api
     module Givings
       class FeesController < ApplicationController
         def calculate
-          command = ::Givings::Card::CalculateStripeGiving.call(value: params[:value],
-                                                                currency: params[:currency])
+          command = ::Givings::Card::CalculateStripeGiving.call(value: value,
+                                                                currency: currency)
 
           if command.success?
             render json: GivingFeeBlueprint.render(command.result), status: :ok
           else
             render_errors(command.errors)
           end
+        end
+
+        private
+
+        def value
+          @value ||= params[:value]
+        end
+
+        def currency
+          @currency ||= params[:currency].to_sym
         end
       end
     end

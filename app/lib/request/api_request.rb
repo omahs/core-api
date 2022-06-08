2d39a8c7cea66ced5b7696e782afa91ca9778183
Nicholas Marques <nnmarques97@gmail.com>
2022-06-08 11:33:49 -0300

    refactor: add RecursiveOpenStruct after cache on ApiRequest

--- app/lib/request/api_request.rb
+++ app/lib/request/api_request.rb
@@ -1,11 +1,11 @@
 module Request
   class ApiRequest
     def self.get(url, expires_in: nil)
-      RedisStore::Cache.find_or_create(key: sanitized_url(url), expires_in: expires_in) do
-        response = HTTParty.get(url)
-
-        RecursiveOpenStruct.new(response)
+      response = RedisStore::Cache.find_or_create(key: sanitized_url(url), expires_in: expires_in) do
+        HTTParty.get(url)
       end
+
+      RecursiveOpenStruct.new(response)
     end
 
     def self.sanitized_url(url)

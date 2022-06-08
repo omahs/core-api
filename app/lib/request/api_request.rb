83ca9e7002c9f0d0f0b1aaa368a636a354977dbd
Nicholas Marques <nnmarques97@gmail.com>
2022-06-08 09:55:45 -0300

    feat: add with_cache block to ApiRequest

--- app/lib/request/api_request.rb
+++ app/lib/request/api_request.rb
@@ -1,9 +1,21 @@
 module Request
   class ApiRequest
-    def self.get(url)
-      response = HTTParty.get(url)
+    def self.get(url, expires_in: nil)
+      with_cache(cache_key: sanitized_url(url), expires_in: expires_in) do
+        response = HTTParty.get(url)
 
-      RecursiveOpenStruct.new(response)
+        RecursiveOpenStruct.new(response)
+      end
+    end
+
+    def self.with_cache(cache_key:, expires_in:, &block)
+      return Rails.cache.fetch(cache_key, expires_in: expires_in, &block) if expires_in
+
+      yield
+    end
+
+    def self.sanitized_url(url)
+      url.gsub(/[^0-9A-Za-z]/, '')
     end
   end
 end

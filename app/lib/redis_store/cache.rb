32b576f82ea7b2294d850334f8017f31e3979eaf
Nicholas Marques <nnmarques97@gmail.com>
2022-06-08 10:09:35 -0300

    refactor: add RedisStore::Cache

---
+++ app/lib/redis_store/cache.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module RedisStore
+  module Cache
+    module_function
+
+    def cache(key:, expires_in:, &block)
+      return Rails.cache.fetch(key, expires_in: expires_in, &block) if expires_in
+
+      yield
+    end
+  end
+end
--- app/lib/request/api_request.rb
+++ app/lib/request/api_request.rb
@@ -1,17 +1,11 @@
 module Request
   class ApiRequest
     def self.get(url, expires_in: nil)
-      with_cache(cache_key: sanitized_url(url), expires_in: expires_in) do
+      RedisStore::Cache.cache(key: sanitized_url(url), expires_in: expires_in) do
         response = HTTParty.get(url)
 
         RecursiveOpenStruct.new(response)
       end
-    end
-
-    def self.with_cache(cache_key:, expires_in:, &block)
-      return Rails.cache.fetch(cache_key, expires_in: expires_in, &block) if expires_in
-
-      yield
     end
 
     def self.sanitized_url(url)

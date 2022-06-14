409b20459adbe39998250089f343ae5dca543f0c
Henrique Barbosa <henrikleod@gmail.com>
2022-06-14 12:52:13 -0300

    refactor: splitting payment methods by using an entities module

---
+++ app/lib/payment/stripe/entities/customer.rb
@@ -0,0 +1,18 @@
+module Payment
+  module Stripe
+    module Entities
+      class Customer
+        def self.create(customer:, payment_method:)
+          ::Stripe::Customer.create({
+            email: customer&.email,
+            name: customer&.name,
+            payment_method: payment_method.id,
+            invoice_settings: {
+              default_payment_method: payment_method.id
+            }
+          })
+        end
+      end
+    end
+  end
+end

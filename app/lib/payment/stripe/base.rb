24f153e3d385ed3aa123a112f71b4dcc9a895f93
Henrique Barbosa <henrikleod@gmail.com>
2022-06-14 14:56:30 -0300

    feat: add an auxiliary constant for payment methods

--- app/lib/payment/stripe/base.rb
+++ app/lib/payment/stripe/base.rb
@@ -1,6 +1,10 @@
 module Payment
   module Stripe
     class Base
+      ALLOWED_PAYMENT_METHODS = {
+        card: 'card'
+      }.freeze
+
       def initialize
         ::Stripe.api_key = RibonCoreApi.config[:stripe][:secret_key]
       end

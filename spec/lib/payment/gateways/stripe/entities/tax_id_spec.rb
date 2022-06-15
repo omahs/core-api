336ca87e49fe72f5d145a6718c7ec6df1a83f0d4
Henrique Barbosa <henrikleod@gmail.com>
2022-06-15 10:54:15 -0300

    feat: taxid addition specs

---
+++ spec/lib/payment/gateways/stripe/entities/tax_id_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Payment::Gateways::Stripe::Entities::TaxId do
+  describe '#add_to_customer' do
+    subject(:taxid_addition_call) do
+      described_class.add_to_customer(stripe_customer: stripe_customer, national_id: national_id)
+    end
+
+    let(:gateway) { Payment::Gateways::Stripe::Base }
+
+    let(:stripe_customer) { OpenStruct.new({ id: 'cus_123' }) }
+    let(:national_id) { '11122233345' }
+
+    let(:method_parameters) do
+      [
+        stripe_customer.id,
+        {
+          type: gateway::ALLOWED_TAXID_REGIONS[:brazil],
+          value: national_id
+        }
+      ]
+    end
+
+    before do
+      allow(::Stripe::Customer).to receive(:create_tax_id)
+    end
+
+    it 'calls the Stripe::PaymentMethod with correct params' do
+      taxid_addition_call
+
+      expect(::Stripe::Customer)
+        .to have_received(:create_tax_id)
+        .with(*method_parameters)
+    end
+  end
+end

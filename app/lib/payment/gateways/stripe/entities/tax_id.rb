2da19eb99d5f3a83c0a5423b6957a8775a4a6200
Henrique Barbosa <henrikleod@gmail.com>
2022-06-15 10:54:38 -0300

    feat: taxid addition class

---
+++ app/lib/payment/gateways/stripe/entities/tax_id.rb
@@ -0,0 +1,19 @@
+module Payment
+  module Gateways
+    module Stripe
+      module Entities
+        class TaxId
+          def self.add_to_customer(stripe_customer:, national_id:)
+            ::Stripe::Customer.create_tax_id(
+              stripe_customer&.id,
+              {
+                type: Base::ALLOWED_TAXID_REGIONS[:brazil],
+                value: national_id
+              }
+            )
+          end
+        end
+      end
+    end
+  end
+end

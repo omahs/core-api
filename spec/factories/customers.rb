33356edf27df5bd4a1ab86a11f94552cb6e98aa7
Henrique Barbosa <henrikleod@gmail.com>
2022-06-15 11:54:24 -0300

    refac: renaming national_id in remaining places

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :customer do
     name { 'a customer' }
     email { 'customer@customer.com' }
-    national_id { '12345678901' }
+    tax_id { '12345678901' }
     customer_keys { { stripe: "cus_#{SecureRandom.uuid}" } }
     association :user, factory: :user
   end

0b195c8b3f36fd7b6932b1459097024769198c69
Henrique Barbosa <henrikleod@gmail.com>
2022-06-20 14:07:53 -0300

    refactor: removing duplicated customer factory

--- spec/factories/customers.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :customer do
-    name { 'a customer' }
-    email { 'customer@customer.com' }
-    tax_id { '12345678901' }
-    customer_keys { { stripe: "cus_#{SecureRandom.uuid}" } }
-    association :user, factory: :user
-  end
-end

d50a349064d5cc5ddd6c4449cc21c9dedd4a22bb
Henrique Barbosa <henrikleod@gmail.com>
2022-06-20 16:50:26 -0300

    feature: customer payments model

---
+++ app/models/customers/customer_payment.rb
@@ -0,0 +1,14 @@
+class CustomerPayments < ApplicationRecord
+  include UuidHelper
+
+  PAYMENT_METHODS = %w[credit_card pix crypto].freeze
+  STATUSES = %w[processing paid failed].freeze
+
+  belongs_to :customer
+  belongs_to :offer
+
+  validates :paid_date, presence: true
+  validates :status, presence: true, inclusion: { in: STATUSES, message: "%{value} is not a valid status" }
+  validates :payment_method, presence: true,
+                             inclusion: { in: PAYMENT_METHODS, message: "%{value} is not a valid payment method" }
+end

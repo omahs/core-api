39e162cca888606cf1615ecb5a90f5fc8d16604e
Henrique Barbosa <henrikleod@gmail.com>
2022-06-20 16:49:56 -0300

    feature: updating database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_15_203835) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_194753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -54,6 +54,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_203835) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "customer_payments", force: :cascade do |t|
+    t.datetime "paid_date"
+    t.string "payment_method"
+    t.string "status"
+    t.uuid "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_customer_payments_on_customer_id"
   end
 
   create_table "customers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|

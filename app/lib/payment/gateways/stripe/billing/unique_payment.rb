e7d3e3d3d33b93381c09c6a9f7f196749464398e
Henrique Barbosa <henrikleod@gmail.com>
2022-06-20 14:58:32 -0300

    feature: stripe unique payment implementation

---
+++ app/lib/payment/gateways/stripe/billing/unique_payment.rb
@@ -0,0 +1,19 @@
+module Payment
+  module Gateways
+    module Stripe
+      module Billing
+        class UniquePayment
+          def self.create(stripe_customer:, stripe_payment_method:, offer:)
+            ::Stripe::PaymentIntent.create({
+              amount: offer.price_cents,
+              currency: offer.currency,
+              payment_method: stripe_payment_method,
+              customer: stripe_customer,
+              confirm: true
+            })
+          end
+        end
+      end
+    end
+  end
+end

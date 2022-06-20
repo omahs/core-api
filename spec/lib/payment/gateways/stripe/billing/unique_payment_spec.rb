1d53361fd296b871bd69a1a3b62302aae0ac4bc1
Henrique Barbosa <henrikleod@gmail.com>
2022-06-20 14:48:22 -0300

    feature: unique payment specs

---
+++ spec/lib/payment/gateways/stripe/billing/unique_payment_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe Payment::Gateways::Stripe::Billing::UniquePayment do
+  describe '#create' do
+    subject(:method_call) { described_class.create(stripe_payment_method: stripe_payment_method,
+                                            stripe_customer: stripe_customer, offer: offer) }
+    
+    let!(:currency) { "brl" }
+    let!(:price_cents) { 100 }
+
+    let(:offer) { create(:offer, price_cents: price_cents, subscription: false ) }
+    let(:stripe_customer) { instance_double(::Stripe::Customer) }
+    let(:stripe_payment_method) { instance_double(::Stripe::PaymentMethod) }
+
+    before do
+      allow(::Stripe::PaymentIntent).to receive(:create)
+    end
+
+    it 'calls the Stripe::PaymentIntent with correct params' do
+      method_call
+
+      expect(::Stripe::PaymentIntent)
+        .to have_received(:create)
+        .with(payment_method: stripe_payment_method,
+              customer: stripe_customer, amount: price_cents,
+              currency: currency, confirm: true)
+    end
+  end
+end

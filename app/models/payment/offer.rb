4f848215356febba3d91f9973bdd7057d4901ba8
Henrique Barbosa <henrikleod@gmail.com>
2022-06-20 14:43:21 -0300

    fix: validation broken if subscription is false

--- app/models/payment/offer.rb
+++ app/models/payment/offer.rb
@@ -2,7 +2,8 @@ class Offer < ApplicationRecord
   has_one :offer_gateway, dependent: :nullify, inverse_of: :offer
   accepts_nested_attributes_for :offer_gateway, allow_destroy: true
   validates :price_cents, numericality: { greater_than_or_equal_to: 0 }
-  validates :currency, :subscription, presence: true
+  validates :currency, presence: true
+  validates :subscription, inclusion: [true, false]
 
   delegate :gateway, to: :offer_gateway
   delegate :external_id, to: :offer_gateway

3775be8c067556497498b49d849d9d3ef7a1b354
Nicholas Marques <nnmarques97@gmail.com>
2022-07-05 15:14:33 -0300

    test: add tests to Web3::Providers::Client

---
+++ spec/lib/web3/providers/client_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Web3::Providers::Client do
+  describe '.create' do
+    subject(:method_call) { described_class.create(network:) }
+
+    let(:network) { Web3::Providers::Networks::MUMBAI }
+
+    before do
+      allow(Eth::Client).to receive(:create).and_return(
+        OpenStruct.new({ max_fee_per_gas: 0,
+                         max_priority_fee_per_gas: 0,
+                         gas_limit: 0 })
+      )
+    end
+
+    it 'calls Eth::Client create with correct param' do
+      method_call
+
+      expect(Eth::Client).to have_received(:create).with(network[:node_url])
+    end
+
+    it 'sets the correct fees' do
+      client = method_call
+
+      expect(client.max_fee_per_gas).to eq described_class::DEFAULT_MAX_FEE_PER_GAS
+      expect(client.max_priority_fee_per_gas).to eq described_class::DEFAULT_MAX_FEE_PER_GAS
+      expect(client.gas_limit).to eq described_class::DEFAULT_GAS_LIMIT
+    end
+  end
+end

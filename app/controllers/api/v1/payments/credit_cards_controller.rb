92ed42c173ed92d6f20ab48fccca66e52d49c101
Henrique Barbosa <henrikleod@gmail.com>
2022-07-05 16:06:08 -0300

    fix: credit card command call

--- app/controllers/api/v1/payments/credit_cards_controller.rb
+++ app/controllers/api/v1/payments/credit_cards_controller.rb
@@ -3,9 +3,10 @@ module Api
     module Payments
       class CreditCardsController < ApplicationController
         include Adapter::Controllers::Payment::CreditCards
+        include ::Givings::Payment::OrderTypes
 
         def create
-          command = ::Givings::Payment::CreateOrder.call(order_params)
+          command = ::Givings::Payment::CreateOrder.call(CreditCard, order_params)
 
           if command.success?
             head :created

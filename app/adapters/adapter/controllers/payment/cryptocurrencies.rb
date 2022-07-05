f870eb0d120a695035ee4ca731bb747f69850a0a
Henrique Barbosa <henrikleod@gmail.com>
2022-07-05 16:10:23 -0300

    feature: crypto payment adapter

---
+++ app/adapters/adapter/controllers/payment/cryptocurrencies.rb
@@ -0,0 +1,21 @@
+module Adapter
+  module Controllers
+    module Payment
+      module Cryptocurrencies
+        private
+
+        def order_params
+          { email: payment_params[:email], payment_method:, user:, amount: payment_params[:amount], transaction_hash: payment_params[:transaction_hash]}
+        end
+
+        def payment_method
+          :crypto
+        end
+        
+        def user
+          @user ||= current_user || User.find_or_create_by(email: payment_params[:email])
+        end
+      end
+    end
+  end
+end

033cdcbc2ff133a1e231ec7f3eaaac50d782ff82
Nicholas Marques <nnmarques97@gmail.com>
2022-07-06 16:36:16 -0300

    refactor: add call_add_balance_command to create_order

--- app/commands/givings/payment/create_order.rb
+++ app/commands/givings/payment/create_order.rb
@@ -27,15 +27,19 @@ module Givings
       private
 
       def success_callback(order, _result)
-        transaction_hash = Givings::CommunityTreasure::AddBalance
-                           .call(amount: order.payment.amount, user_identifier: order.customer.email).result
+        transaction_hash = call_add_balance_command
         order.payment.update(status: :paid)
-        order.payment
-             .create_customer_payment_blockchain(treasure_entry_status: :processing, transaction_hash:)
+        order.payment.create_customer_payment_blockchain(treasure_entry_status: :processing,
+                                                         transaction_hash:)
       end
 
       def failure_callback(order, _result)
         order.payment.update(status: :failed)
+      end
+
+      def call_add_balance_command
+        Givings::CommunityTreasure::AddBalance
+          .call(amount: order.payment.amount, user_identifier: order.customer.email).result
       end
     end
   end

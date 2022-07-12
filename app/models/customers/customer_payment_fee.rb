f485acfa624017a1498d6e1f3e75de2ef4dc7391
Nicholas Marques <nnmarques97@gmail.com>
2022-07-12 14:44:28 -0300

    feat: add fee methods to customer_payment_fee

--- app/models/customers/customer_payment_fee.rb
+++ app/models/customers/customer_payment_fee.rb
@@ -1,3 +1,19 @@
 class CustomerPaymentFee < ApplicationRecord
   belongs_to :customer_payment
+
+  def service_fee_cents
+    card_fee_cents + crypto_fee_cents
+  end
+
+  def card_fee
+    card_fee_cents / 100.0
+  end
+
+  def crypto_fee
+    crypto_fee_cents / 100.0
+  end
+
+  def service_fee
+    service_fee_cents / 100.0
+  end
 end
--- spec/models/customer_payment_fee_spec.rb
+++ spec/models/customer_payment_fee_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe CustomerPaymentFee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:customer_payment_fee) { build(:customer_payment_fee, card_fee_cents: 100, crypto_fee_cents: 150) }
+
+  describe 'validations' do
+    it { is_expected.to belong_to(:customer_payment) }
+  end
+
+  describe '#service_fee_cents' do
+    it 'returns the sum of card_fee and crypto_fee' do
+      expect(customer_payment_fee.service_fee_cents).to eq 250
+    end
+  end
+
+  describe '#service_fee' do
+    it 'returns the service fee total amount' do
+      expect(customer_payment_fee.service_fee).to eq 2.50
+    end
+  end
+
+  describe '#card_fee' do
+    it 'returns the card fee total amount' do
+      expect(customer_payment_fee.card_fee).to eq 1.0
+    end
+  end
+
+  describe '#crypto_fee' do
+    it 'returns the crypto fee total amount' do
+      expect(customer_payment_fee.crypto_fee).to eq 1.5
+    end
+  end
 end

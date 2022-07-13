0b5771c31a56e4a558be5f417e539d8a8ace500b
Nicholas Marques <nnmarques97@gmail.com>
2022-07-13 08:51:34 -0300

    chore: update person_payment_fees factory

--- spec/factories/person_payment_fees.rb
+++ spec/factories/person_payment_fees.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :person_payment_fee do
-    card_fee_cents { 1 }
-    crypto_fee_cents { 1 }
-    person_payment { nil }
+    card_fee_cents { 100 }
+    crypto_fee_cents { 100 }
+    association :person_payment, factory: :person_payment
   end
 end

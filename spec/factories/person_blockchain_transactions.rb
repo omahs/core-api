e0e0c236f8e21f71942e7092232bc6c409f9c1d8
Nicholas Marques <nnmarques97@gmail.com>
2022-07-13 12:25:39 -0300

    test: update person_payment_services blockchain transaction test

--- spec/factories/person_blockchain_transactions.rb
+++ spec/factories/person_blockchain_transactions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :person_blockchain_transaction do
     treasure_entry_status { 0 }
-    person_payment { nil }
+    association :person_payment, factory: :person_payment
     transaction_hash { '0x44d5e936dad202ec600b6a6a5252ef32c0e29bb9a21b179e348d2e8029cc1c86' }
   end
 end

3757a71952cba5b026bd5aedf29ea88a159be3c7
Henrique Barbosa <henrikleod@gmail.com>
2022-07-18 10:06:13 -0300

    feature: updating database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_114105) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_125629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -210,6 +210,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_114105) do
     t.integer "default_ticket_value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sources", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "integration_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["integration_id"], name: "index_sources_on_integration_id"
+    t.index ["user_id"], name: "index_sources_on_user_id"
   end
 
   create_table "user_donation_stats", force: :cascade do |t|

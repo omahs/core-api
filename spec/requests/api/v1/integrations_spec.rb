ca8f5da6a2a7607c69158ccda27a2b4cd7c03f70
Henrique Barbosa <henrikleod@gmail.com>
2022-07-21 14:09:51 -0300

    refactor: renaming existing specs

--- spec/requests/api/v1/integrations_spec.rb
+++ spec/requests/api/v1/integrations_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Api::V1::Integrations', type: :request do
       create_list(:integration, 2)
     end
 
-    it 'returns a list integrations' do
+    it 'returns a list of integrations' do
       request
 
       expect_response_collection_to_have_keys(%w[created_at id updated_at logo name url wallet_address])
@@ -20,7 +20,7 @@ RSpec.describe 'Api::V1::Integrations', type: :request do
 
     let(:integration) { create(:integration) }
 
-    it 'returns a list of integrations' do
+    it 'returns a single integration' do
       request
 
       expect_response_to_have_keys(%w[created_at id updated_at logo name url wallet_address])

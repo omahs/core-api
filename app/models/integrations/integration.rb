60c3bf2b0538ae312df6d6765c932009455cd011
Henrique Barbosa <henrikleod@gmail.com>
2022-07-22 10:32:16 -0300

    fix: rubocop issues

--- app/models/integrations/integration.rb
+++ app/models/integrations/integration.rb
@@ -1,6 +1,6 @@
 class Integration < ApplicationRecord
   has_one_attached :logo
-  enum status: [:active, :inactive]
+  enum status: { active: 0, inactive: 1 }
 
   validates :url, :wallet_address, :name, :logo, presence: true
 

7ee7c927041eb3efeccccc2ee939cd69dae73f6c
Henrique Barbosa <henrikleod@gmail.com>
2022-07-22 10:27:39 -0300

    refactor: integration model improvements

--- app/models/integrations/integration.rb
+++ app/models/integrations/integration.rb
@@ -1,5 +1,10 @@
 class Integration < ApplicationRecord
   has_one_attached :logo
+  enum status: [:active, :inactive]
 
   validates :url, :wallet_address, :name, :logo, presence: true
+
+  def integration_address
+    "https://dapp.ribon.io/integration/#{id}"
+  end
 end

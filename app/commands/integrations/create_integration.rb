66d97ee0c5fb8ab10bff2fc2a631870b8947da82
Henrique Barbosa <henrikleod@gmail.com>
2022-07-28 16:42:03 -0300

    feature: integration command

---
+++ app/commands/integrations/create_integration.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Integrations
+  class CreateIntegration < ApplicationCommand
+    prepend SimpleCommand
+    include Web3
+
+    attr_reader :integration
+
+    def initialize(name:, status:)
+      @integration = Integration.new(name:, status:, unique_address:)
+    end
+
+    def call
+      with_exception_handle do
+        if integration.save
+          IntegrationWallet.create!(encrypted_wallet)
+        end
+      end
+    end
+
+    private
+
+    def unique_address
+      SecureRandom.uuid
+    end
+
+    def encrypted_wallet
+      public_key, private_key = Providers::Keys.generate_keypair
+
+      encrypted_key = Utils::Cipher.encrypt(private_key)
+
+      {
+        public_key:,
+        encrypted_private_key: encode(encrypted_key.cipher_text),
+        private_key_iv: encode(encrypted_key.iv),
+        integration:
+      }
+    end
+
+    def encode(text)
+      Base64.strict_encode64(text)
+    end
+  end
+end

7d50622cbc407211b88b1fc72f7f3571c6baba62
Henrique Barbosa <henrikleod@gmail.com>
2022-07-29 11:08:58 -0300

    fix: rubocop issues

--- spec/requests/api/v1/integrations_spec.rb
+++ spec/requests/api/v1/integrations_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe 'Api::V1::Integrations', type: :request do
     it 'returns a list of integrations' do
       request
 
-      expect_response_collection_to_have_keys(%w[created_at id updated_at name status unique_address integration_address integration_wallet])
+      expect_response_collection_to_have_keys(%w[created_at id updated_at name status unique_address
+                                                 integration_address integration_wallet])
     end
   end
 
@@ -23,7 +24,8 @@ RSpec.describe 'Api::V1::Integrations', type: :request do
     it 'returns a single integration' do
       request
 
-      expect_response_to_have_keys(%w[created_at id updated_at name status unique_address integration_address integration_wallet])
+      expect_response_to_have_keys(%w[created_at id updated_at name status unique_address integration_address
+                                      integration_wallet])
     end
   end
 end

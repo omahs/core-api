ddd7728371ac50bb9140ceeaf8179748de69db59
Henrique Barbosa <henrikleod@gmail.com>
2022-07-29 11:29:14 -0300

    fix: rubocop issues

--- spec/factories/integration_wallets.rb
+++ spec/factories/integration_wallets.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :integration_wallet do
     public_key { '0x27e26b8fdc3b84d6c3c9c5b7eb60dcbe2963bdba' }
-    encrypted_private_key { "f10e5b7f78587398999f9ce0d233de46396a1d5891d0048a3f6a9f39898ac3df" }
+    encrypted_private_key { 'f10e5b7f78587398999f9ce0d233de46396a1d5891d0048a3f6a9f39898ac3df' }
     private_key_iv { '849346d327c9' }
   end
 end

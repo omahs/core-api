8e07c46ccabb73c4f49f9061c3d8ff3cc777593f
Henrique Barbosa <henrikleod@gmail.com>
2022-07-29 11:20:52 -0300

    refactor: add integration wallet to the integration factory

--- spec/factories/integrations.rb
+++ spec/factories/integrations.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { 'Renner' }
     status { :active }
     unique_address { 'f7be8d80-2406-4cb0-82eb-849346d327c9' }
+    integration_wallet { build(:integration_wallet) }
   end
 end

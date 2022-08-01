f24de7ffdb9ccbe524f0d36327a6d3c4a68ad8b1
Henrique Barbosa <henrikleod@gmail.com>
2022-08-01 12:07:34 -0300

    feature: integration specs

--- spec/requests/api/v1/integrations_spec.rb
+++ spec/requests/api/v1/integrations_spec.rb
@@ -16,6 +16,29 @@ RSpec.describe 'Api::V1::Integrations', type: :request do
     end
   end
 
+  describe 'POST /create' do
+    subject(:request) { post "/api/v1/integrations", params: }
+
+    let(:params) do
+      {
+        name: "Ribon",
+        status: "0"
+      }
+    end
+
+    let!(:result) { create(:integration) }
+
+    before do
+      mock_command(klass: Integrations::CreateIntegration, result:)
+      request
+    end
+
+    it 'returns a single integration' do
+      expect_response_to_have_keys(%w[created_at id updated_at name status unique_address integration_address
+                                      integration_wallet])
+    end
+  end
+
   describe 'GET /show' do
     subject(:request) { get "/api/v1/integrations/#{integration.id}" }
 

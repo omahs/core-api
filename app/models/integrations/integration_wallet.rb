1a8f48073f8918e972b5233f21728c0c4a52ca92
Nicholas Marques <nnmarques97@gmail.com>
2022-08-08 13:33:32 -0300

    feat: add #private_key to integration_wallet

--- app/models/integrations/integration_wallet.rb
+++ app/models/integrations/integration_wallet.rb
@@ -2,4 +2,11 @@ class IntegrationWallet < ApplicationRecord
   belongs_to :integration
 
   validates :public_key, :encrypted_private_key, :private_key_iv, presence: true
+
+  def private_key
+    decrypted_pk = Base64.decode64(encrypted_private_key)
+    decrypted_pk_iv = Base64.decode64(private_key_iv)
+
+    Web3::Utils::Cipher.decrypt(decrypted_pk, decrypted_pk_iv).plain_text
+  end
 end

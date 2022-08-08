868221adecc0ddaeedce248addfe7f016837f03d
Nicholas Marques <nnmarques97@gmail.com>
2022-08-08 15:23:06 -0300

    fix: update encrypted key on tests

--- spec/factories/integration_wallets.rb
+++ spec/factories/integration_wallets.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :integration_wallet do
-    encrypted_key = Web3::Utils::Cipher.encrypt('3ecf3128ad6df6257e693368e8860cd59b5ceee5f6550796efe841d4ccaed1c2')
     public_key { '0xdbce37c4431e394d7892a053cbc39a411bbf25d2' }
-    encrypted_private_key { Base64.strict_encode64(encrypted_key.cipher_text) }
-    private_key_iv { Base64.strict_encode64(encrypted_key.iv) }
+    after(:build) do |iw|
+      ek = Web3::Utils::Cipher.encrypt('3ecf3128ad6df6257e693368e8860cd59b5ceee5f6550796efe841d4ccaed1c2')
+      iw.encrypted_private_key = Base64.strict_encode64(ek.cipher_text)
+      iw.private_key_iv = Base64.strict_encode64(ek.iv)
+    end
   end
 end

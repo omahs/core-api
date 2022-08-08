94c64a66fb3c1b9c7dc66edb2f1c71c61080878b
Nicholas Marques <nnmarques97@gmail.com>
2022-08-08 14:43:32 -0300

    fix: add cipher encrypt to integration_wallet factory

--- spec/factories/integration_wallets.rb
+++ spec/factories/integration_wallets.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :integration_wallet do
     public_key { '0xdbce37c4431e394d7892a053cbc39a411bbf25d2' }
     # rubocop:disable Layout/LineLength
-    encrypted_private_key { Base64.strict_encode64("\x84\xA9\xC3\x8F\xC9+\xA6\xDBQ\xB0\xC7=\x82\xD2\xA9-%\xAC\xF9\x95xr\x92+\x99w\x0F\x1C\x18\xF0\xBC\x1D\xF1\xC6bD\x17y\xFDr\xFF1\xF7\xBD\xBF\x16;\";t.Z$ \x7F\xAACR\x1A\x981\x1E&\td\xC4_\xC6\xD9\xBB)\xFBW\xB8\x06\xA6{\xC0#\xE4") }
+    encrypted_private_key { Base64.strict_encode64(Web3::Utils::Cipher.encrypt('3ecf3128ad6df6257e693368e8860cd59b5ceee5f6550796efe841d4ccaed1c2').cipher_text) }
+    private_key_iv { Base64.strict_encode64(Web3::Utils::Cipher.encrypt('3ecf3128ad6df6257e693368e8860cd59b5ceee5f6550796efe841d4ccaed1c2').iv) }
     # rubocop:enable Layout/LineLength
-    private_key_iv { Base64.strict_encode64(")B\x82\x06\x89\xEB\x87`+;\xCD\xCD\xF5]J\xBF") }
   end
 end

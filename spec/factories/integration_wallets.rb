41ccf44b41f6378631c75fcacc703700387e7402
Nicholas Marques <nnmarques97@gmail.com>
2022-08-08 15:00:12 -0300

    fix: update encrypted key on tests

--- spec/factories/integration_wallets.rb
+++ spec/factories/integration_wallets.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
+  encrypted_key = Web3::Utils::Cipher.encrypt('3ecf3128ad6df6257e693368e8860cd59b5ceee5f6550796efe841d4ccaed1c2')
+
   factory :integration_wallet do
     public_key { '0xdbce37c4431e394d7892a053cbc39a411bbf25d2' }
-    # rubocop:disable Layout/LineLength
-    encrypted_private_key { Base64.strict_encode64(Web3::Utils::Cipher.encrypt('3ecf3128ad6df6257e693368e8860cd59b5ceee5f6550796efe841d4ccaed1c2').cipher_text) }
-    private_key_iv { Base64.strict_encode64(Web3::Utils::Cipher.encrypt('3ecf3128ad6df6257e693368e8860cd59b5ceee5f6550796efe841d4ccaed1c2').iv) }
-    # rubocop:enable Layout/LineLength
+    encrypted_private_key { Base64.strict_encode64(encrypted_key.cipher_text) }
+    private_key_iv { Base64.strict_encode64(encrypted_key.iv) }
   end
 end
--- spec/models/integration_wallet_spec.rb
+++ spec/models/integration_wallet_spec.rb
@@ -12,16 +12,7 @@ RSpec.describe IntegrationWallet, type: :model do
 
   describe '#private_key' do
     let(:private_key_string) { '3ecf3128ad6df6257e693368e8860cd59b5ceee5f6550796efe841d4ccaed1c2' }
-    # rubocop:disable Layout/LineLength
-    let(:private_key) { "\x84\xA9\xC3\x8F\xC9+\xA6\xDBQ\xB0\xC7=\x82\xD2\xA9-%\xAC\xF9\x95xr\x92+\x99w\x0F\x1C\x18\xF0\xBC\x1D\xF1\xC6bD\x17y\xFDr\xFF1\xF7\xBD\xBF\x16;\";t.Z$ \x7F\xAACR\x1A\x981\x1E&\td\xC4_\xC6\xD9\xBB)\xFBW\xB8\x06\xA6{\xC0#\xE4" }
-    # rubocop:enable Layout/LineLength
-    let(:private_key_iv) { ")B\x82\x06\x89\xEB\x87`+;\xCD\xCD\xF5]J\xBF" }
-    let(:encrypted_private_key) { Base64.strict_encode64(private_key) }
-    let(:encrypted_private_key_iv) { Base64.strict_encode64(private_key_iv) }
-
-    let(:integration_wallet) do
-      build(:integration_wallet, encrypted_private_key:, private_key_iv: encrypted_private_key_iv)
-    end
+    let(:integration_wallet) { build(:integration_wallet) }
 
     it 'returns the integration private key decoded' do
       expect(integration_wallet.private_key).to eq(private_key_string)

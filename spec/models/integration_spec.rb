ced54af162bc567fa4ad99f7086c04ec9bd4bb29
Henrique Barbosa <henrikleod@gmail.com>
2022-08-09 15:57:27 -0300

    feature: specs for all the new model methods

--- spec/models/integration_spec.rb
+++ spec/models/integration_spec.rb
@@ -16,4 +16,40 @@ RSpec.describe Integration, type: :model do
       expect(integration.integration_address).to eq("https://dapp.ribon.io/integration/#{integration.unique_address}")
     end
   end
+
+  describe '#ticket_availability' do
+    context 'when ticket_availability_in_minutes is present' do
+      let(:integration) { create(:integration, ticket_availability_in_minutes: 10) }
+
+      it 'returns the ticket availability in minutes' do
+        expect(integration.ticket_availability).to eq('10 minutes')
+      end
+    end
+
+    context 'when ticket_availability_in_minutes is not present' do
+      let(:integration) { create(:integration) }
+
+      it 'returns the ticket availability in minutes' do
+        expect(integration.ticket_availability).to eq('Everyday at midnight')
+      end
+    end
+  end
+
+  describe '#daily_availability?' do
+    context 'when ticket_availability_in_minutes is present' do
+      let(:integration) { create(:integration, ticket_availability_in_minutes: 10) }
+
+      it 'returns false' do
+        expect(integration.daily_availability?).to be_falsey
+      end
+    end
+
+    context 'when ticket_availability_in_minutes is not present' do
+      let(:integration) { create(:integration) }
+
+      it 'returns true' do
+        expect(integration.daily_availability?).to be_truthy
+      end
+    end
+  end
 end

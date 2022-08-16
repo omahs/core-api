1bfca2f9900e926622f563f225f1206823097f53
Henrique Barbosa <henrikleod@gmail.com>
2022-08-16 18:44:24 -0300

    fix: lint

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   def health
     database_ping = NonProfit.first&.id&.present?
-    redis_ping    = RibonCoreApi.redis.ping == "PONG"
+    redis_ping    = RibonCoreApi.redis.ping == 'PONG'
 
     render json: { api: true, redis: redis_ping, database: database_ping }, status: :ok
   rescue StandardError => e

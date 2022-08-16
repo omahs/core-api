af0ca28013ceba377fe80e17a4f5760a42eabaec
Nicholas Marques <nnmarques97@gmail.com>
2022-08-16 17:02:59 -0300

    fix: remove impact by ticket float conversion

--- app/models/non_profits/non_profit_impact.rb
+++ app/models/non_profits/non_profit_impact.rb
@@ -4,7 +4,7 @@ class NonProfitImpact < ApplicationRecord
   validates :usd_cents_to_one_impact_unit, :start_date, :end_date, presence: true
 
   def impact_by_ticket
-    RibonConfig.default_ticket_value.to_f / usd_cents_to_one_impact_unit
+    RibonConfig.default_ticket_value / usd_cents_to_one_impact_unit
   rescue StandardError
     0
   end

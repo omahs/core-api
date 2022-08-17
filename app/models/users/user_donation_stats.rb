9278597c4e63c2ca1258a962517ce0cc0f3ef469
Henrique Barbosa <henrikleod@gmail.com>
2022-08-17 14:09:21 -0300

    feature: period validation for integrations

--- app/models/users/user_donation_stats.rb
+++ app/models/users/user_donation_stats.rb
@@ -1,13 +1,17 @@
 class UserDonationStats < ApplicationRecord
   belongs_to :user
 
-  def can_donate?
-    return true if next_donation_at.nil?
+  def can_donate?(integration)
+    return true if next_donation_at(integration).nil?
 
-    Time.zone.now >= next_donation_at
+    Time.zone.now >= next_donation_at(integration)
   end
 
-  def next_donation_at
+  def next_donation_at(integration)
+    time_interval = integration.ticket_availability_in_minutes
+
+    return last_donation_at + time_interval.minutes if time_interval.present?
+  
     last_donation_at&.next_day&.beginning_of_day
   end
 end

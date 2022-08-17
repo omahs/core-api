cc6a66b929918a636144f500f296acf5bb8caade
Henrique Barbosa <henrikleod@gmail.com>
2022-08-17 16:32:45 -0300

    refactor: use utc timezone on specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,4 +38,8 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:suite) do
+    Time.zone = "UTC"
+  end
 end

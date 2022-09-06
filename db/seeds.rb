92c1a0909e54a46b506da4810e0a13fcc39f11fa
Yan Galli <yangalli@hotmail.com>
2022-09-06 16:02:21 -0300

    fix: delete logs in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,13 @@
 unless Rails.env.production?
-  puts "Creating Ribon Config..."
   ribon_config = FactoryBot.create(:ribon_config)
-  puts "Ribon Config created."
 
-  puts "Creating non profits..."
   non_profit = FactoryBot.create(:non_profit, :with_impact)
-  puts "Non profits created."
 
-  puts "Creating integrations..."
   integration = FactoryBot.create(:integration)
-  puts "Integrations created."
 
-  puts "Creating admin..."
   admin = FactoryBot.create(:admin)
-  puts "Admin created."
 
-  puts "Creating test user..."
   user = FactoryBot.create(:user)
-  puts "Test user created."
 
-  puts "Creating mumbai chain..."
   chain = FactoryBot.create(:chain)
-  puts "Mumbai chain created."
 end

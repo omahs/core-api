1d1d422e0d3beaf264379e1eae4c2d9f86daf7c8
thaisribon <thais@ribon.io>
2022-09-08 15:00:52 -0300

    feat: change on non profit migration

--- db/migrate/20220906183657_add_status_to_non_profits.rb
+++ db/migrate/20220906183657_add_status_to_non_profits.rb
@@ -1,5 +1,5 @@
 class AddStatusToNonProfits < ActiveRecord::Migration[7.0]
   def change
-    add_column :non_profits, :status, :integer, default: 1
+    add_column :non_profits, :status, :integer, default: 0
   end
 end

886cb734de19e883b9d75f1ef5c1afa725a49c4a
Henrique Barbosa <henrikleod@gmail.com>
2022-09-12 14:51:24 -0300

    refactor: remove integration relationship from migration

--- db/migrate/20220908143101_create_pools.rb
+++ db/migrate/20220908143101_create_pools.rb
@@ -3,7 +3,6 @@ class CreatePools < ActiveRecord::Migration[7.0]
     create_table :pools do |t|
       t.string :address
       t.references :token, null: false, foreign_key: true
-      t.references :integration, null: false, foreign_key: true
 
       t.timestamps
     end

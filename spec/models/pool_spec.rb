64e0040b1734751b6360424f51465887a3f56221
Henrique Barbosa <henrikleod@gmail.com>
2022-09-12 14:51:52 -0300

    refactor: remove integration relationship from specs

--- spec/models/pool_spec.rb
+++ spec/models/pool_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Pool, type: :model do
 
     it { is_expected.to validate_presence_of(:address) }
     it { is_expected.to belong_to(:token) }
-    it { is_expected.to belong_to(:integration) }
   end
 
   describe '#chain' do

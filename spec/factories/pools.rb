afc16cdc96468b37ccc9e8a9ce9aaaeafac9f43d
Henrique Barbosa <henrikleod@gmail.com>
2022-09-12 14:51:39 -0300

    refactor: remove integration relationship from factory

--- spec/factories/pools.rb
+++ spec/factories/pools.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :pool do
     address { '0xE00000000000000000000000000000000000000000000000' }
-    integration { build(:integration) }
     token { build(:token) }
   end
 end

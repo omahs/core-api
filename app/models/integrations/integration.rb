234dffbb4815ba7ff1c0cf28086a096446440188
Nicholas Marques <nnmarques97@gmail.com>
2022-09-12 10:37:08 -0300

    feat: add #find_by_id_or_unique_address to Integration

--- app/models/integrations/integration.rb
+++ app/models/integrations/integration.rb
@@ -6,6 +6,12 @@ class Integration < ApplicationRecord
 
   validates :name, :unique_address, presence: true
 
+  def self.find_by_id_or_unique_address(id_or_address)
+    return find_by(unique_address: id_or_address) if id_or_address.to_s.valid_uuid?
+
+    find id_or_address
+  end
+
   def integration_address
     "#{base_url}#{unique_address}"
   end
---
+++ lib/ext/string.rb
@@ -0,0 +1,9 @@
+class String
+  def valid_uuid?
+    uuid_regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+
+    return true if uuid_regex.match?(to_s.downcase)
+
+    false
+  end
+end

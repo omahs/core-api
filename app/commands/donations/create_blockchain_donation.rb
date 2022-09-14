b09efe212fc67a8d46058c41c3aa82db85c87759
Nicholas Marques <nnmarques97@gmail.com>
2022-09-14 14:13:43 -0300

    feat: add CreateBlockchainDonation command

---
+++ app/commands/donations/create_blockchain_donation.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Donations
+  class CreateBlockchainDonation < ApplicationCommand
+    prepend SimpleCommand
+
+    attr_reader :donation
+
+    CENTS_FACTOR = 0.01
+
+    def initialize(donation:)
+      @donation = donation
+    end
+
+    def call
+      transaction_hash = create_blockchain_donation
+      create_donation_blockchain_transaction(transaction_hash)
+    end
+
+    private
+
+    def create_blockchain_donation
+      amount = ticket_value * CENTS_FACTOR
+      non_profit_wallet_address = non_profit.wallet_address
+
+      ribon_contract.donate_through_integration(donation_pool_address:,
+                                                non_profit_wallet_address:,
+                                                user: user.email, amount:, sender_key:)
+    end
+
+    def create_donation_blockchain_transaction(transaction_hash)
+      donation.create_donation_blockchain_transaction(transaction_hash:, chain:)
+    end
+
+    def sender_key
+      @sender_key ||= integration.integration_wallet&.private_key
+    end
+
+    def ticket_value
+      @ticket_value ||= RibonConfig.default_ticket_value
+    end
+
+    def ribon_contract
+      @ribon_contract ||= Web3::Contracts::RibonContract.new(chain:)
+    end
+
+    def chain
+      @chain ||= Chain.default
+    end
+
+    def non_profit
+      @non_profit ||= donation.non_profit
+    end
+
+    def user
+      @user ||= donation.user
+    end
+
+    def donation_pool_address
+      '0x841cad54aaeAdFc9191fb14EB09232af8E20be0F'
+    end
+  end
+end

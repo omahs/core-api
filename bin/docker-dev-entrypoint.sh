ee1e94c1840cbfb6b0ecd4e75a6244a09b5fe1a3
thaisribon <thais@ribon.io>
2022-09-15 15:25:02 -0300

    feat: dockerfile

--- bin/docker-dev-entrypoint.sh
+++ bin/docker-dev-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-bundle exec rails db:setup
-
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
+
+bin/rails db:environment:set RAILS_ENV=development
+bundle exec rails db:setup
+bundle exec rails s -b 0.0.0.0 -p 8080
 
 exec "$@"

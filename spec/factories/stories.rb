3e750d03522ac5bb6f9e73bd47432327e50f98ac
Henrique Barbosa <henrikleod@gmail.com>
2022-09-15 15:35:01 -0300

    feature: story annotations

--- spec/factories/stories.rb
+++ spec/factories/stories.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: stories
+#
+#  id            :bigint           not null, primary key
+#  active        :boolean
+#  description   :text
+#  position      :integer
+#  title         :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  non_profit_id :bigint           not null
+#
 FactoryBot.define do
   factory :story do
     title { 'Story' }

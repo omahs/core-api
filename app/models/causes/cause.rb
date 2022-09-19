691f5c755011b8738fed285efe274710c51d2d84
Henrique Barbosa <henrikleod@gmail.com>
2022-09-19 11:59:24 -0300

    rollback: poll and causes association in the model

--- app/models/causes/cause.rb
+++ app/models/causes/cause.rb
@@ -6,10 +6,8 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  pool_id    :bigint           not null
 #
 class Cause < ApplicationRecord
-  belongs_to :pool
   has_many :non_profits
 
   validates :name, presence: true

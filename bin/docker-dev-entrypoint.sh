575137de639119a3d917b549667048849ca9cbc3
thaisribon <thais@ribon.io>
2022-09-19 13:47:20 -0300

    feat: fix database create

--- bin/docker-dev-entrypoint.sh
+++ bin/docker-dev-entrypoint.sh
@@ -5,8 +5,10 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+bundle exec rails db:create
 bin/rails db:environment:set RAILS_ENV=development
-bundle exec rails db:setup
+bundle exec rails db:migrate
+bundle exec rails db:seed
 bundle exec rails s -b 0.0.0.0 -p 8080
 
 exec "$@"

2f6774891d451b2a916462ab3fe36892c876e0b8
Yan Galli <yangalli@hotmail.com>
2022-09-20 09:34:02 -0300

    refactor: update google_api_url to a method

--- app/commands/manager/set_user_manager_tokens.rb
+++ app/commands/manager/set_user_manager_tokens.rb
@@ -12,14 +12,19 @@ module Manager
 
     def call
       with_exception_handle do
-        url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{id_token}"
-        response = Request::ApiRequest.get(url)
+        response = Request::ApiRequest.get(google_api_url)
         @user_manager = UserManager.create_user_for_google(response)
         tokens = @user_manager.create_new_auth_token
         @user_manager.save
 
         tokens
       end
+    end
+
+    private
+
+    def google_api_url
+      "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{id_token}"
     end
   end
 end

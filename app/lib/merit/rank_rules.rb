e341500789269cc41e683667967e599e07fe15e5
Nicholas Marques <nnmarques97@gmail.com>
2022-09-26 12:03:59 -0300

    fix: add Merit module to RankRules

--- app/lib/merit/rank_rules.rb
+++ app/lib/merit/rank_rules.rb
@@ -1,9 +1,11 @@
-class RankRules
-  include Merit::RankRulesMethods
+module Merit
+  class RankRules
+    include Merit::RankRulesMethods
 
-  def initialize
-    set_rank level: 1, to: User.all do |user|
-      user.donations.count > 1
+    def initialize
+      set_rank level: 1, to: User.all do |user|
+        user.donations.count > 1
+      end
     end
   end
 end

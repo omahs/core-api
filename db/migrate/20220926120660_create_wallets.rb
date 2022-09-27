d759dbb0ed9fd61602a60657554ec528f8316bc0
Nicholas Marques <nnmarques97@gmail.com>
2022-09-27 14:21:34 -0300

    chore: uncomment rename wallet create wallets migrations

--- db/migrate/20220926120660_create_wallets.rb
+++ db/migrate/20220926120660_create_wallets.rb
@@ -1,14 +1,14 @@
 class CreateWallets < ActiveRecord::Migration[7.0]
   def change
-    # create_table :wallets do |t|
-    #   t.string :public_key, unique: true
-    #   t.string :encrypted_private_key, unique: true
-    #   t.string :private_key_iv, unique: true
-    #   t.integer :status
-    #   t.references :owner, polymorphic: true, null: false, index: true
-    #   t.string :type, null: false
-    #
-    #   t.timestamps
-    # end
+    create_table :wallets do |t|
+      t.string :public_key, unique: true
+      t.string :encrypted_private_key, unique: true
+      t.string :private_key_iv, unique: true
+      t.integer :status
+      t.references :owner, polymorphic: true, null: false, index: true
+      t.string :type, null: false
+
+      t.timestamps
+    end
   end
 end

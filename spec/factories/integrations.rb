e588830cac96b79dc64bf22cb5011e7c48421c09
let <let@MacBook-Air-de-let.local>
2022-09-28 13:59:19 -0300

    feat: add logo in tests

--- spec/factories/integrations.rb
+++ spec/factories/integrations.rb
@@ -15,6 +15,7 @@ FactoryBot.define do
     name { 'Renner' }
     status { :active }
     unique_address { 'f7be8d80-2406-4cb0-82eb-849346d327c9' }
+    logo { nil }
     ticket_availability_in_minutes { nil }
     integration_wallet { build(:integration_wallet) }
   end

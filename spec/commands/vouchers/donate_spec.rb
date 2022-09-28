56e59e0e2aa5867ec7fb23768e125c811319df28
Nicholas Marques <nnmarques97@gmail.com>
2022-09-28 10:26:22 -0300

    test: add tests to vouchers donate spec

--- spec/commands/vouchers/donate_spec.rb
+++ spec/commands/vouchers/donate_spec.rb
@@ -6,7 +6,7 @@ describe Vouchers::Donate do
   describe '.call' do
     subject(:command) { described_class.call(integration:, donation_command:, external_id:) }
 
-    context 'when no error occurs' do
+    context 'when the voucher is valid' do
       let(:donation_command) { Donations::Donate.new(integration:, non_profit:, user:) }
       let(:external_id) { 'external_id' }
       let(:integration) { build(:integration) }
@@ -17,11 +17,57 @@ describe Vouchers::Donate do
 
       before do
         allow(donation_command).to receive(:call).and_return(donation_command_double)
-        create(:ribon_config, default_ticket_value: 100)
       end
 
-      it 'creates a voucher' do
-        expect(command.result).to be_an_instance_of Voucher
+      it 'returns success' do
+        expect(command).to be_success
+      end
+
+      it 'calls the donation command' do
+        command
+
+        expect(donation_command).to have_received(:call)
+      end
+
+      it 'creates a voucher with correct params' do
+        voucher = command.result
+
+        expect(voucher).to be_an_instance_of Voucher
+        expect(voucher.external_id).to eq external_id
+        expect(voucher.integration).to eq integration
+        expect(voucher.donation).to eq donation
+      end
+    end
+
+    context 'when the voucher is invalid' do
+      let(:donation_command) { Donations::Donate.new(integration:, non_profit:, user:) }
+      let(:external_id) { nil }
+      let(:integration) { build(:integration) }
+      let(:non_profit) { build(:non_profit) }
+      let(:user) { build(:user) }
+      let(:donation) { build(:donation) }
+      let(:donation_command_double) { command_double(klass: Donations::Donate, result: donation) }
+
+      before do
+        allow(donation_command).to receive(:call).and_return(donation_command_double)
+      end
+
+      it 'returns failure' do
+        expect(command).to be_failure
+      end
+
+      it 'does not call the donation_command' do
+        command
+
+        expect(donation_command).not_to have_received(:call)
+      end
+
+      it 'does not create a voucher' do
+        expect { command }.not_to change(Voucher, :count)
+      end
+
+      it 'returns an error message' do
+        expect(command.errors).to eq({ message: [I18n.t('donations.blocked_message')] })
       end
     end
   end

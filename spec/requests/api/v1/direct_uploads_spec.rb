720ebc693e65f8acf874a39d21118da6ec974b7d
thaisribon <thais@ribon.io>
2022-09-28 19:53:58 -0300

    feat: lint

--- spec/requests/api/v1/direct_uploads_spec.rb
+++ spec/requests/api/v1/direct_uploads_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe 'Api::V1::DirectUploads', type: :request do
 
     let(:params) do
       { blob: {
-          filename: "teste.jpeg",
-          byte_size: 649004,
-          checksum: "FN224+vkcKWKUQ0A2iVeTQ==",
-          content_type: "image/jpeg"
-        }
-      }
+        filename: 'teste.jpeg',
+        byte_size: 649_004,
+        checksum: 'FN224+vkcKWKUQ0A2iVeTQ==',
+        content_type: 'image/jpeg'
+      } }
     end
 
-    it 'should create a new blob on active storage' do
+    it 'creates a new blob on active storage' do
       request
 
-      expect_response_to_have_keys(%w[attachable_sgid byte_size checksum content_type created_at direct_upload filename id key metadata service_name service_url signed_id])
+      expect_response_to_have_keys(%w[attachable_sgid byte_size checksum content_type created_at direct_upload
+                                      filename id key metadata service_name service_url signed_id])
     end
   end
 end

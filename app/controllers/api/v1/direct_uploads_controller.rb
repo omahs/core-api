81bdf3ce8b1343ad0b7e3ba574cb81ba5cb85147
let <let@MacBook-Air-de-let.local>
2022-09-28 14:11:17 -0300

    fix: adjust lint

--- app/controllers/api/v1/direct_uploads_controller.rb
+++ app/controllers/api/v1/direct_uploads_controller.rb
@@ -5,7 +5,9 @@ module Api
 
       def create
         blob = ActiveStorage::Blob.create_before_direct_upload!(filename: blob_args[:filename],
-                                                                byte_size: blob_args[:byte_size], checksum: blob_args[:checksum], content_type: blob_args[:content_type])
+                                                                byte_size: blob_args[:byte_size],
+                                                                checksum: blob_args[:checksum],
+                                                                content_type: blob_args[:content_type])
 
         render json: direct_upload_json(blob)
       end
@@ -18,10 +20,11 @@ module Api
       end
 
       def direct_upload_json(blob)
-        blob.as_json(root: false, methods: :signed_id).merge(service_url: url_for(blob)).merge(direct_upload: {
-                                                                                                 url: blob.service_url_for_direct_upload,
-                                                                                                 headers: blob.service_headers_for_direct_upload
-                                                                                               })
+        blob.as_json(root: false, methods: :signed_id).merge(service_url: url_for(blob))
+            .merge(direct_upload: {
+                     url: blob.service_url_for_direct_upload,
+                     headers: blob.service_headers_for_direct_upload
+                   })
       end
     end
   end

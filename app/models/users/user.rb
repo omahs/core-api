5fd91bf6102dc86cfbcc10ca91179dda5807c4f0
Nicholas Marques <nnmarques97@gmail.com>
2022-09-28 14:37:21 -0300

    fix: remove incorrect annotations from user

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -4,10 +4,8 @@
 #
 #  id         :bigint           not null, primary key
 #  email      :string
-#  level      :integer          default(0)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  sash_id    :integer
 #
 class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: true }, format: { with: URI::MailTo::EMAIL_REGEXP }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,8 @@
 #
 #  id         :bigint           not null, primary key
 #  email      :string
-#  level      :integer          default(0)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  sash_id    :integer
 #
 FactoryBot.define do
   factory :user do

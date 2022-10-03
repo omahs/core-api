6e458c9fcaa40c60743f0bec192fe4f39804e9e1
thaisribon <thais@ribon.io>
2022-10-03 14:10:06 -0300

    feat: mobility attributes

--- app/blueprints/integration_task_blueprint.rb
+++ app/blueprints/integration_task_blueprint.rb
@@ -1,3 +1,7 @@
 class IntegrationTaskBlueprint < Blueprinter::Base
   fields :link, :link_address, :description
+
+  field(:mobility_attributes) do |object|
+    IntegrationTask.mobility_attributes
+  end
 end

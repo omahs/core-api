373f5e870c5e18427e52a8ac809a909e2e1e6f98
thaisribon <thais@ribon.io>
2022-10-03 14:11:48 -0300

    feat: lint

--- app/blueprints/integration_task_blueprint.rb
+++ app/blueprints/integration_task_blueprint.rb
@@ -1,7 +1,7 @@
 class IntegrationTaskBlueprint < Blueprinter::Base
   fields :link, :link_address, :description
 
-  field(:mobility_attributes) do |object|
+  field(:mobility_attributes) do |_object|
     IntegrationTask.mobility_attributes
   end
 end

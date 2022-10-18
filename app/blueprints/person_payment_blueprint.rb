f762891f18243474fcbaf813036b612c6cb3c432
Yan Galli <yangalli@hotmail.com>
2022-10-18 14:46:46 -0300

    fix: update PersonPayment blueprint

--- app/blueprints/person_payment_blueprint.rb
+++ app/blueprints/person_payment_blueprint.rb
@@ -1,6 +1,8 @@
 class PersonPaymentBlueprint < Blueprinter::Base
   identifier :id
+
   fields :paid_date, :crypto_amount, :amount_cents, :payment_method, :status
+
   association :offer, blueprint: OfferBlueprint
   association :person, blueprint: PersonBlueprint
 end

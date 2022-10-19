df75167165da19ef1d85e23ee336a4087fe4a1e8
Yan Galli <yangalli@hotmail.com>
2022-10-19 15:58:01 -0300

    feat: add pagination to PersonPayment

--- app/controllers/api/v1/person_payments_controller.rb
+++ app/controllers/api/v1/person_payments_controller.rb
@@ -2,9 +2,33 @@ module Api
   module V1
     class PersonPaymentsController < ApplicationController
       def index
-        @person_payments = PersonPayment.all
+        sortable = params[:sort].present? ? "#{params[:sort]} #{sort_direction}" : 'created_at desc'
 
-        render json: PersonPaymentBlueprint.render(@person_payments)
+        @person_payments = PersonPayment.order(sortable).page(@page).per(@per)
+
+        render json: PersonPaymentBlueprint.render(@person_payments, total_items:, page:, total_pages:)
+      end
+
+      private
+
+      def sort_direction
+        %w[asc desc].include?(params[:sort_dir]) ? params[:sort_dir] : 'asc'
+      end
+
+      def total_pages
+        @person_payments.page(@page).total_pages
+      end
+
+      def total_items
+        @total_items = @person_payments.count
+      end
+
+      def page
+        @page = params[:page] || 1
+      end
+
+      def per
+        @per = params[:per] || 10
       end
     end
   end

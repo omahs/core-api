1a14881d6f6f6ff23f15e6ced7eae21c88dceabd
thaisribon <thais@ribon.io>
2022-10-20 08:43:28 -0300

    fix: update webhook url

---
+++ app/commands/integrations/update_integration.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Integrations
+  class UpdateIntegration < ApplicationCommand
+    prepend SimpleCommand
+    include Web3
+
+    attr_reader :integration_params
+
+    def initialize(integration_params)
+      @integration_params = integration_params
+    end
+
+    def call
+      with_exception_handle do
+        integration = Integration.find integration_params[:id]
+        integration.update(update_integration_params)
+        if integration_params[:webhook_url].present?
+          if !integration.webhook_url
+            create_integration_webhook(integration)
+          else
+            update_integration_webhook(integration)
+          end
+        end
+        integration
+      end
+    end
+
+    private
+
+    def update_integration_params
+      integration_params.except(:webhook_url)
+    end
+
+    def create_integration_webhook(integration)
+      IntegrationWebhook.create(url: integration_params[:webhook_url], integration:)
+    end
+
+    def update_integration_webhook(integration)
+      IntegrationWebhook.where(integration:).last.update(url: integration_params[:webhook_url])
+    end
+  end
+end

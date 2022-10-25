d01d3a9b20a79db22077c0b60ab07c6bcb4f028a
let <let@MacBook-Air-de-let.local>
2022-10-25 15:36:25 -0300

    feat: add external_id to person payments blueprint

--- app/blueprints/person_payment_blueprint.rb
+++ app/blueprints/person_payment_blueprint.rb
@@ -1,7 +1,7 @@
 class PersonPaymentBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :paid_date, :crypto_amount, :amount_cents, :payment_method, :status
+  fields :paid_date, :crypto_amount, :amount_cents, :payment_method, :status, :external_id
 
   field :total_items do |_, options|
     options[:total_items]

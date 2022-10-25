2961270403bcdb5498a0e19b490c72b915ca5e14
Nicholas Marques <nnmarques97@gmail.com>
2022-10-25 14:05:19 -0300

    test: add UpdateApiOnlyDonations test

--- spec/commands/donations/update_api_only_donations_spec.rb
+++ spec/commands/donations/update_api_only_donations_spec.rb
@@ -4,6 +4,22 @@ require 'rails_helper'
 
 describe Donations::UpdateApiOnlyDonations do
   describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    subject(:command) { described_class.call }
+
+    let!(:api_only_donation) { create(:donation) }
+    let!(:donation_with_blockchain_transaction) { create(:donation) }
+
+    before do
+      allow(Donations::CreateBlockchainDonation).to receive(:call)
+      create(:donation_blockchain_transaction, donation: donation_with_blockchain_transaction)
+    end
+
+    it 'calls the Donations::CreateBlockchainDonation with failed transactions donations' do
+      command
+
+      expect(Donations::CreateBlockchainDonation).to have_received(:call).with(donation: api_only_donation)
+      expect(Donations::CreateBlockchainDonation)
+        .not_to have_received(:call).with(donation: donation_with_blockchain_transaction)
+    end
   end
 end

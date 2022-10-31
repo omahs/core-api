cfa26487169b2650e5a21dfdf696849450603cc6
Henrique Barbosa <henrikleod@gmail.com>
2022-10-31 15:30:14 -0300

    feat: add main image to causes blueprint

--- app/blueprints/cause_blueprint.rb
+++ app/blueprints/cause_blueprint.rb
@@ -3,6 +3,10 @@ class CauseBlueprint < Blueprinter::Base
 
   fields :updated_at, :created_at, :name
 
+  field(:main_image) do |object|
+    ImagesHelper.image_url_for(object.main_image)
+  end
+
   view :minimal do
     excludes :created_at, :updated_at
   end

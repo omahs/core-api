cf2b3f7e886488e236b00cae51064fb3c670f6fe
Nathiely Macedo <nathielymm@gmail.com>
2022-11-09 09:05:28 -0300

    feat: add tests to active method

--- spec/requests/api/v1/causes_spec.rb
+++ spec/requests/api/v1/causes_spec.rb
@@ -27,4 +27,30 @@ RSpec.describe 'Api::V1::Causes', type: :request do
       expect_response_to_have_keys(%w[created_at id updated_at name cover_image main_image pools active])
     end
   end
+
+  describe '#active' do
+    context 'when the non_profits is empty' do
+      let(:cause) { create(:cause, non_profits: []) }
+
+      it 'returns false' do
+        expect(cause.active).to be_falsey
+      end
+    end
+
+    context 'when the cause has non_profits but its inactive' do
+      let(:cause) { create(:cause, non_profits: [create(:non_profit, status: :inactive)]) }
+
+      it 'returns false' do
+        expect(cause.active).to be_falsey
+      end
+    end
+
+    context 'when the cause has active non_profits associated' do
+      let(:cause) { create(:cause, non_profits: [create(:non_profit, status: :active)]) }
+
+      it 'returns false' do
+        expect(cause.active).to be_truthy
+      end
+    end
+  end
 end

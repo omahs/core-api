f9822b9879edc807ea292599e7dfbdd489954392
let <let@MacBook-Air-de-let.local>
2022-11-09 16:48:34 -0300

    fix: fix linter

--- app/blueprints/offer_blueprint.rb
+++ app/blueprints/offer_blueprint.rb
@@ -1,8 +1,8 @@
 class OfferBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :currency, :subscription, :price_cents, :price_value, :active, :title, :position_order, :external_id, :gateway,
-         :created_at, :updated_at
+  fields :currency, :subscription, :price_cents, :price_value, :active, :title, :position_order,
+         :external_id, :gateway, :created_at, :updated_at
 
   field :price do |object|
     Money.new(object.price_cents, object.currency).format

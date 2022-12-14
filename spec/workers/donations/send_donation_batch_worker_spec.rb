decd2d838661782c0f5d7f7a8c6c430b9d53978f
thaisribon <thais@ribon.io>
2022-12-14 16:31:06 -0300

    feat: donations queue

--- spec/workers/donations/send_donation_batch_worker_spec.rb
+++ spec/workers/donations/send_donation_batch_worker_spec.rb
@@ -35,10 +35,10 @@ RSpec.describe Donations::SendDonationBatchWorker, type: :worker do
       end.to change(described_class.jobs, :size).from(0).to(1)
     end
 
-    it 'expects to add one job in the default queue' do
+    it 'expects to add one job in the donations queue' do
       expect do
         described_class.perform_async
-      end.to change(Sidekiq::Queues['default'], :size).by(1)
+      end.to change(Sidekiq::Queues['donations'], :size).by(1)
     end
   end
 end

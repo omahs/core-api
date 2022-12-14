37f12876d1f8a2121ca763af3dbd207a1ee89e9b
thaisribon <thais@ribon.io>
2022-12-14 16:06:07 -0300

    feat: send donation batch job

---
+++ app/jobs/donations/send_donation_batch_job.rb
@@ -0,0 +1,15 @@
+module Donations
+  class SendDonationBatchJob < ApplicationJob
+
+    def perform
+      Integration.all.each do |integration|
+        NonProfit.all.each do |non_profit|
+          batch = Donations::CreateDonationsBatch.call(integration:, non_profit:).result
+          CreateBatchBlockchainDonationJob.perform_later(non_profit:, integration:, batch:) if batch
+        end
+      end
+    rescue StandardError => e
+      Reporter.log(e, { message: e.message })
+    end
+  end
+end

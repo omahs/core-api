9891475ac3ac838b908c44dffe62ee14ec0c3d26
Let <let@MacBook-Pro-de-Let.local>
2022-12-20 18:41:28 -0300

    fix: adjust size of main_image

--- app/blueprints/site_non_profits_blueprint.rb
+++ app/blueprints/site_non_profits_blueprint.rb
@@ -2,8 +2,8 @@ class SiteNonProfitsBlueprint < Blueprinter::Base
   fields :name
 
   field(:main_image) do |object|
-    ImagesHelper.image_url_for(object.main_image, variant: { resize_to_fit: [450, 450],
-                                                             saver: { quality: 95 }, format: :jpg })
+    ImagesHelper.image_url_for(object.main_image, variant: { resize_to_fit: [800, 800],
+                                                        saver: { quality: 95 }, format: :jpg })
   end
 
   field :description do |non_profit, options|

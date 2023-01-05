84e83d38c7f6f65d1e2a8d0194cdab8c4467c38e
Henrique Barbosa <henrikleod@gmail.com>
2023-01-05 16:51:38 -0300

    feat: count by date ranges

--- app/services/service/donations/statistics.rb
+++ app/services/service/donations/statistics.rb
@@ -15,6 +15,14 @@ module Service
         @total_donors ||= donations.distinct.count(:user_id)
       end
 
+      def donations_in_date_intervals
+        count_in_sub_ranges(:total_donations_between)
+      end
+
+      def donors_in_date_intervals
+        count_in_sub_ranges(:total_donors_between)
+      end
+
       def impact_per_non_profit
         non_profits.map { |non_profit| format_impacts(non_profit) }
                    .select { |result| (result[:impact]).positive? }
@@ -31,6 +39,34 @@ module Service
       end
 
       private
+
+      def count_in_sub_ranges(counter, full_intervals = 5)
+        days_spacing = calculate_days_spacing(full_intervals)
+
+        (start_date.to_i..end_date.to_i).step(days_spacing).map do |date|
+          range_start_date, range_end_date = date_range(date, days_spacing)
+
+          {
+            initial_date: range_start_date.strftime('%d/%m/%Y'),
+            count: send(counter, range_start_date, range_end_date)
+          }
+        end
+      end
+
+      def calculate_days_spacing(full_intervals)
+        days_spacing = (end_date - start_date) / full_intervals
+        days_spacing < 1.day ? 1.day : days_spacing
+      end
+
+      def date_range(initial_timestamp, days_spacing)
+        range_start_date = Time.zone.at(initial_timestamp)
+        range_end_date = Time.zone.at(initial_timestamp + days_spacing)
+
+        [
+          range_start_date,
+          range_end_date > Time.zone.now ? Time.zone.now : range_end_date
+        ]
+      end
 
       def format_impacts(non_profit)
         { non_profit:, impact: impact_sum_by_non_profit(non_profit) }
@@ -52,6 +88,22 @@ module Service
 
       def total_usd_cents_donated_for(non_profit)
         donations.where(non_profit:).sum(&:value)
+      end
+
+      def total_donations_between(start_date, end_date)
+        donations.where('created_at >= ? AND created_at <= ?', start_date, end_date).count
+      end
+
+      def total_donors_between(start_date, end_date)
+        donations.where('created_at >= ? AND created_at <= ?', start_date, end_date).distinct.count(:user_id)
+      end
+
+      def start_date
+        @start_date ||= donations.order(:created_at).first.created_at
+      end
+
+      def end_date
+        @end_date ||= donations.order(:created_at).last.created_at
       end
 
       def non_profits

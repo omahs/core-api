ef131906a15da8c91897684b5af2e440111ca33a
mendesmcmg <claraplanejamento@gmail.com>
2023-01-05 17:01:45 -0300

    fix: linter

--- spec/commands/donations/blockchain_transactions/update_failed_transactions_spec.rb
+++ spec/commands/donations/blockchain_transactions/update_failed_transactions_spec.rb
@@ -35,7 +35,7 @@ describe Donations::BlockchainTransactions::UpdateFailedTransactions do
 
     it 'doesnt call the Donations::CreateBatchBlockchainDonation with successfull transactions donations' do
       command
-      
+
       success_transactions.each do |transaction|
         expect(Donations::CreateBatchBlockchainDonation)
           .not_to have_received(:call).with(

76653943eb7f0710301e457a8885995468988963
mendesmcmg <claraplanejamento@gmail.com>
2023-01-09 15:52:53 -0300

    test: adds history blueprint test

--- spec/blueprints/donation_blueprint_spec.rb
+++ spec/blueprints/donation_blueprint_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 RSpec.describe DonationBlueprint do
   let(:donation) { create(:donation) }
-  let(:donation_blueprint) { DonationBlueprint.render(donation) }
+  let(:donation_blueprint) { described_class.render(donation) }
 
-  it 'should render donation' do
+  it 'renders donation' do
     expect(donation_blueprint).to include(:value.to_s)
     expect(donation_blueprint).to include(:impact.to_s)
     expect(donation_blueprint).to include(:impact_value.to_s)

a11403cd54dc92b3e7b942f39e280a29d5b2dcaa
Thais Correa <thais@ribon.io>
2023-01-17 09:18:59 -0300

    fix: add background image to non profit params

--- app/controllers/api/v1/non_profits_controller.rb
+++ app/controllers/api/v1/non_profits_controller.rb
@@ -41,7 +41,7 @@ module Api
       private
 
       def non_profit_params
-        params.permit(:id, :name, :status, :impact_description, :wallet_address, :logo, :main_image, :cause_id,
+        params.permit(:id, :name, :status, :impact_description, :wallet_address, :logo, :main_image, :background_image, :cause_id,
                       stories_attributes: %i[id title description position active image])
       end
 

f612b7f747118037cb593a31738f33a4143fb624
Let <let@MacBook-Pro-de-Let.local>
2023-01-18 10:29:47 -0300

    feat: add donor recipient to blueprint

--- app/blueprints/non_profit_impacts_blueprint.rb
+++ app/blueprints/non_profit_impacts_blueprint.rb
@@ -1,5 +1,5 @@
 class NonProfitImpactsBlueprint < Blueprinter::Base
-  fields :id, :end_date, :start_date, :usd_cents_to_one_impact_unit
+  fields :id, :end_date, :start_date, :usd_cents_to_one_impact_unit, :donor_recipient
 
   field :measurement_unit do |object|
     object.measurement_unit || 'quantity_without_decimals'

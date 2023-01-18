057d9f39eb2d0971517a6261b659c2e0d44babcd
Henrique Barbosa <henrikleod@gmail.com>
2023-01-18 10:46:19 -0300

    feat: add impact description

--- app/blueprints/non_profit_impacts_blueprint.rb
+++ app/blueprints/non_profit_impacts_blueprint.rb
@@ -1,5 +1,5 @@
 class NonProfitImpactsBlueprint < Blueprinter::Base
-  fields :id, :end_date, :start_date, :usd_cents_to_one_impact_unit, :donor_recipient
+  fields :id, :end_date, :start_date, :usd_cents_to_one_impact_unit, :donor_recipient, :impact_description
 
   field :measurement_unit do |object|
     object.measurement_unit || 'quantity_without_decimals'

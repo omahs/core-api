588aefc7fbbb6c43a127c1f0cad7792ad9f8ee25
Henrique Barbosa <henrikleod@gmail.com>
2023-01-18 08:52:30 -0300

    fix: naming

--- app/commands/givings/impact/calculate_impact_to_non_profit.rb
+++ app/commands/givings/impact/calculate_impact_to_non_profit.rb
@@ -15,14 +15,14 @@ module Givings
 
       def call
         with_exception_handle do
-          { impact: impact_service.impact, rounded_impact: impact_service.rounded_impact, impact_unit: }
+          { impact: impact_service.impact, rounded_impact: impact_service.rounded_impact, measurement_unit: }
         end
       end
 
       private
 
-      def impact_unit
-        @impact_unit ||= non_profit.impact_for&.measurement_unit || 'quantity_without_decimals'
+      def measurement_unit
+        @measurement_unit ||= non_profit.impact_for&.measurement_unit || 'quantity_without_decimals'
       end
 
       def impact_service

a9f605ea6dd455b89c0763bb82b8a04145c62e64
Thais Correa <thais@ribon.io>
2023-01-23 10:00:33 -0300

    fix: test

--- app/commands/donations/create_blockchain_donation.rb
+++ app/commands/donations/create_blockchain_donation.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# replaced by the batch
+
 module Donations
   class CreateBlockchainDonation < ApplicationCommand
     prepend SimpleCommand
--- spec/jobs/donations/retry_donations_job_spec.rb
+++ spec/jobs/donations/retry_donations_job_spec.rb
@@ -6,14 +6,12 @@ RSpec.describe Donations::RetryDonationsJob, type: :job do
 
     before do
       allow(Donations::UpdateProcessingDonations).to receive(:call)
-      allow(Donations::UpdateApiOnlyDonations).to receive(:call)
       allow(Donations::UpdateFailedDonations).to receive(:call)
       perform_job
     end
 
     it 'calls the retries commands' do
       expect(Donations::UpdateProcessingDonations).to have_received(:call)
-      expect(Donations::UpdateApiOnlyDonations).to have_received(:call)
       expect(Donations::UpdateFailedDonations).to have_received(:call)
     end
   end

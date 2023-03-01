123321ca4b5315a798b1702922d8d01a7a07e82c
Henrique Barbosa <henrikleod@gmail.com>
2023-03-01 16:00:29 -0300

    feat: article annotations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id           :bigint           not null, primary key
+#  published_at :datetime
+#  title        :string
+#  visible      :boolean
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  author_id    :bigint           not null
+#
 class Article < ApplicationRecord
   belongs_to :author
   has_one_attached :image
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,18 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id           :bigint           not null, primary key
+#  published_at :datetime
+#  title        :string
+#  visible      :boolean
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  author_id    :bigint           not null
+#
 FactoryBot.define do
   factory :article do
-    author { nil }
+    author { build(:author) }
     title { "My article" }
     published_at { "2023-03-01 15:42:30" }
     visible { false }

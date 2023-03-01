95e240cd926d28b91c823d182863e2a193d9a5df
Henrique Barbosa <henrikleod@gmail.com>
2023-03-01 10:54:03 -0300

    feat: news and author blueprints

---
+++ app/blueprints/news_blueprint.rb
@@ -0,0 +1,11 @@
+class NewsBlueprint < Blueprinter::Base
+  identifier :id
+
+  fields :updated_at, :created_at, :title, :published_at, :visible
+
+  field(:image_url) do |object|
+    ImagesHelper.image_url_for(object.image)
+  end
+
+  association :author, blueprint: AuthorBlueprint, view: :minimal
+end

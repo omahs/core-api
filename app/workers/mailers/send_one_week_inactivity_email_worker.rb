af3e915e86c6e6d47927b0057431b5df700464fd
mendesmcmg <claraplanejamento@gmail.com>
2023-03-02 11:57:58 -0300

    chore: adds condition for only sending in prod

--- app/workers/mailers/send_one_week_inactivity_email_worker.rb
+++ app/workers/mailers/send_one_week_inactivity_email_worker.rb
@@ -4,6 +4,8 @@ module Mailers
     sidekiq_options queue: :mailers, retry: 3
 
     def perform(*_args)
+      return unless ENV['RAILS_ENV'] == 'production'
+
       SendOneWeekInactivityEmailJob.perform_later
     rescue StandardError => e
       Reporter.log(error: e, extra: { message: e.message })

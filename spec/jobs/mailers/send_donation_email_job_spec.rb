c4df9e02bec95154f666f9d8bc2172f68c2d0e7a
Nicholas Marques <devnicknish@gmail.com>
2023-03-03 09:28:29 -0300

    test: update SendDonationEmailJob specs

--- spec/jobs/mailers/send_donation_email_job_spec.rb
+++ spec/jobs/mailers/send_donation_email_job_spec.rb
@@ -4,15 +4,35 @@ RSpec.describe Mailers::SendDonationEmailJob, type: :job do
   describe '#perform' do
     subject(:perform_job) { described_class.perform_now(donation:) }
 
-    let(:donation) { create(:donation) }
+    let(:user) { create(:user) }
+    let(:donation) { create(:donation, user:) }
 
     before do
-      allow(Mailers::SendDonationEmailJob).to receive(:perform_later)
-      perform_job
+      allow(SendgridWebMailer).to receive(:send_email).and_return(OpenStruct.new({ deliver_later: nil }))
     end
 
-    it 'calls the send donation email job' do
-      expect(Mailers::SendDonationEmailJob).to have_received(:perform_later).with(donation:)
+    context 'when it is a donation on one of the entrypoints' do
+      it 'calls the send email function with correct arguments' do
+        perform_job
+
+        expect(SendgridWebMailer).to have_received(:send_email)
+          .with({ dynamic_template_data: {},
+                  language: user.language,
+                  receiver: user.email,
+                  template_name: 'user_donated_1_tickets_template_id' })
+      end
+    end
+
+    context 'when it is not a donation entrypoint' do
+      before do
+        create_list(:donation, 2, user:)
+      end
+
+      it 'does not call the function to send the email' do
+        perform_job
+
+        expect(SendgridWebMailer).not_to have_received(:send_email)
+      end
     end
   end
 end

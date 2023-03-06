b1ebc8432fd036fe2a7c72ff3cfe145dd9dce18c
Nicholas Marques <devnicknish@gmail.com>
2023-03-06 11:51:42 -0300

    feat: add dynamic templates to contribution emails

--- app/jobs/mailers/send_contribution_reactivation_email_job.rb
+++ app/jobs/mailers/send_contribution_reactivation_email_job.rb
@@ -11,15 +11,18 @@ module Mailers
     private
 
     def send_email(user, contribution)
-      SendgridWebMailer.send_email(receiver: user.email,
-                                   dynamic_template_data: {
-                                     giving_value: giving_value(contribution),
-                                     giving_value_added: giving_value_added(contribution),
-                                     receiver_name: contribution.receiver.name,
-                                     current_date: Time.zone.now.strftime('%d/%m/%Y')
-                                   },
-                                   template_name: "contribution_#{contribution.receiver_type}_template_id",
-                                   language: user.language).deliver_later
+      SendgridWebMailer
+        .send_email(receiver: user.email,
+                    dynamic_template_data:
+                      {
+                        giving_value: giving_value(contribution),
+                        giving_value_added: giving_value_added(contribution),
+                        receiver_name: contribution.receiver.name,
+                        current_date: Time.zone.now.strftime('%d/%m/%Y'),
+                        impact: impact(contribution.receiver)
+                      },
+                    template_name: "contribution_#{contribution.receiver_type&.underscore}_template_id",
+                    language: user.language).deliver_later
     end
 
     def giving_value(contribution)
@@ -28,6 +31,15 @@ module Mailers
 
     def giving_value_added(contribution)
       Money.new(contribution.amount_cents * 0.02, contribution.currency&.to_sym).format
+    end
+
+    def impact(non_profit)
+      return unless non_profit.class.to_s == 'NonProfit'
+
+      ::Impact::Normalizer.new(
+        non_profit,
+        non_profit.impact_by_ticket
+      ).normalize.join(' ')
     end
   end
 end

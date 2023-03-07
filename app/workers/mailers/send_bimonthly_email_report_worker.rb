1cd15f9a689f38adebacf8956639a82582eb1c95
Yan Galli <yangalli@hotmail.com>
2023-03-07 11:14:17 -0300

    feat: add worker to send bimonthly report email

--- app/workers/mailers/send_bimonthly_email_report_worker.rb
+++ app/workers/mailers/send_bimonthly_email_report_worker.rb
@@ -4,10 +4,12 @@ module Mailers
     sidekiq_options queue: :donations
 
     def perform(*_args)
-      users = User.all
-      users.find_in_batches(batch_size: 2_000) do |batch|
-        if user.last_donation_at.between?(3.months.ago, 2.months.ago)
-          Mailers::SendFreeBimonthlyEmailReport.perform_later(donation:)
+      User.find_in_batches(batch_size: 2_000) do |batch|
+        batch.each do |user|
+          if user&.last_donation_at&.between?(3.months.ago, 2.months.ago)
+            Mailers::HandleBimonthlyEmailReportJob.perform_later(user:)
+          end
+        end
       end
     rescue StandardError => e
       Reporter.log(error: e, extra: { message: e.message })
---
+++ spec/workers/mailers/send_bimonthly_email_report_worker_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+require 'sidekiq/testing'
+
+RSpec.describe Mailers::SendBimonthlyEmailReportWorker, type: :worker do
+  describe '#perform' do
+    let(:user1) { create(:user) }
+    let(:user3) { create(:user) }
+    let(:user_stats1) { create(:user_donation_stats, last_donation_at: 1.month.ago) }
+    let(:user_stats3) { create(:user_donation_stats, last_donation_at: 4.months.ago) }
+
+    before do
+      allow(Mailers::HandleBimonthlyEmailReportJob).to receive(:perform_later)
+    end
+
+    it 'sends free bimonthly email report to users with last donation between 3 and 2 months ago' do
+      create(:user)
+      create(:user_donation_stats, last_donation_at: 2.months.ago)
+
+      described_class.new.perform
+
+      expect(Mailers::HandleBimonthlyEmailReportJob).to have_received(:perform_later)
+        .with(user: an_instance_of(User))
+    end
+
+    it 'does not send a bimonthly email report to users with last donation smaller than 3 and 2 months ago' do
+      described_class.new.perform
+
+      expect(Mailers::HandleBimonthlyEmailReportJob).not_to have_received(:perform_later).with(user: user_stats1)
+    end
+
+    it 'does not send a bimonthly email report to users with last donation higher than 3 and 2 months ago' do
+      described_class.new.perform
+
+      expect(Mailers::HandleBimonthlyEmailReportJob).not_to have_received(:perform_later).with(user: user_stats3)
+    end
+
+    it 'logs any errors raised during execution' do
+      allow(User).to receive(:find_in_batches).and_raise(StandardError)
+      allow(Reporter).to receive(:log)
+
+      described_class.new.perform
+
+      expect(Reporter).to have_received(:log)
+    end
+  end
+
+  describe '.perform_async' do
+    it 'expects to enqueue a job' do
+      expect do
+        described_class.perform_async
+      end.to change(described_class.jobs, :size).from(0).to(1)
+    end
+
+    it 'expects to add one job in the donations queue' do
+      expect do
+        described_class.perform_async
+      end.to change(Sidekiq::Queues['donations'], :size).by(1)
+    end
+  end
+end

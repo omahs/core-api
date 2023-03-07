d706441b88ba442272e82e88913968d61fcc1a53
Henrique Barbosa <henrikleod@gmail.com>
2023-03-07 15:39:46 -0300

    refactor: use pure ruby to do the humanization

--- app/lib/formatters/date.rb
+++ app/lib/formatters/date.rb
@@ -1,15 +1,28 @@
 module Formatters
   class Date
-    extend ::ActionView::Helpers::DateHelper
-
-    def self.in_words_for(date)
-      return formatted_date(date) if (1.week.ago..Time.zone.now).cover?(date)
-
-      date.strftime('%d %B')
+    def self.in_words_for(start_time)
+      difference_in_minutes = ((Time.zone.now - start_time) / 60).round
+      
+      case difference_in_minutes
+      when 0
+        I18n.t('date.time_units.half_minute')
+      when 1..59
+        pluralize(difference_in_minutes, 'minute')
+      when 60..1439
+        pluralize((difference_in_minutes / 60), 'hour')
+      when 1440..10079
+        pluralize((difference_in_minutes / 1440), 'day')
+      else
+        pluralize((difference_in_minutes / 10080), 'week')
+      end
     end
 
-    def self.formatted_date(date)
-      I18n.t('date.past', date: time_ago_in_words(date))
+    private
+
+    def self.pluralize(count, singular)
+      key = count == 1 ? singular : singular.pluralize
+    
+      "#{count} #{I18n.t("date.time_units.#{key}")}"
     end
   end
 end

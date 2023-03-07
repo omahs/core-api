cfb5e70dca9e3634ca8b4f8bc8fa94d649942fff
Nicholas Marques <devnicknish@gmail.com>
2023-03-07 09:43:39 -0300

    test: update check_for_contribution_reactivation_worker specs

--- app/workers/mailers/check_for_contribution_reactivation_worker.rb
+++ app/workers/mailers/check_for_contribution_reactivation_worker.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Mailers
-  class CheckForContributionReactivation
+  class CheckForContributionReactivationWorker
     include Sidekiq::Worker
     sidekiq_options queue: :mailers
 

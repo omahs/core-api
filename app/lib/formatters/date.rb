c8770357db173a6ad5a292943d0a73172883d213
Henrique Barbosa <henrikleod@gmail.com>
2023-03-07 15:54:21 -0300

    refactor: split into 2 methods

--- app/lib/formatters/date.rb
+++ app/lib/formatters/date.rb
@@ -2,27 +2,29 @@ module Formatters
   class Date
     def self.in_words_for(start_time)
       difference_in_minutes = ((Time.zone.now - start_time) / 60).round
-      
-      case difference_in_minutes
+
+      format_by(difference_in_minutes)
+    end
+
+    def self.format_by(difference)
+      case difference
       when 0
         I18n.t('date.time_units.half_minute')
       when 1..59
-        pluralize(difference_in_minutes, 'minute')
+        pluralize(difference, 'minute')
       when 60..1439
-        pluralize((difference_in_minutes / 60), 'hour')
-      when 1440..10079
-        pluralize((difference_in_minutes / 1440), 'day')
+        pluralize((difference / 60), 'hour')
+      when 1440..10_079
+        pluralize((difference / 1440), 'day')
       else
-        pluralize((difference_in_minutes / 10080), 'week')
+        pluralize((difference / 10_080), 'week')
       end
     end
 
-    private
-
     def self.pluralize(count, singular)
       key = count == 1 ? singular : singular.pluralize
-    
-      "#{count} #{I18n.t("date.time_units.#{key}")}"
+
+      I18n.t("date.time_units.#{key}", count:).to_s
     end
   end
 end

174d17efa30b5714e24f11e5adc2261c24108ea9
Henrique Barbosa <henrikleod@gmail.com>
2023-03-14 15:31:57 -0300

    fix: rubocop complaints

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -65,7 +65,8 @@ RSpec.describe 'Api::V1::Users', type: :request do
       it 'returns the user' do
         request
 
-        expect_response_to_have_keys %w[created_at id email updated_at last_donation_at last_donated_cause user_completed_tasks]
+        expect_response_to_have_keys %w[created_at id email updated_at last_donation_at last_donated_cause
+                                        user_completed_tasks]
       end
     end
 

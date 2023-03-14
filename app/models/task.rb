fea35c1d15f240a834ad4c53847e7c0254a4102a
Henrique Barbosa <henrikleod@gmail.com>
2023-03-14 15:13:20 -0300

    feat: annotate task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  actions    :text
+#  rules      :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Task < ApplicationRecord
+  has_many :user_completed_tasks
 end

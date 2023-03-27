6e6a51dacdb052ad29084b6524d5471196cf5da5
Thais Correa <thais@ribon.io>
2023-03-27 14:56:01 -0300

    fix: temporary drop donation_contributions

--- db/migrate/20230327151405_create_donation_contributions.rb
+++ db/migrate/20230327151405_create_donation_contributions.rb
@@ -1,5 +1,7 @@
 class CreateDonationContributions < ActiveRecord::Migration[7.0]
   def change
+    drop_table :donation_contributions, if_exists: true
+    
     create_table :donation_contributions do |t|
       t.references :contribution, null: false, foreign_key: true
       t.references :donation, null: false, foreign_key: true

5902016045087716364e61253382729c6ef72560
Nicholas Marques <devnicknish@gmail.com>
2023-03-27 09:24:04 -0300

    test: update contribution_fee_calculator_service_spec

--- spec/factories/ribon_configs.rb
+++ spec/factories/ribon_configs.rb
@@ -15,5 +15,6 @@ FactoryBot.define do
     default_ticket_value { 100 }
     default_chain_id { 0x13881 }
     contribution_fee_percentage { 20.0 }
+    minimum_contribution_chargeable_fee_cents { 10 }
   end
 end
--- spec/services/service/contributions/contribution_fee_calculator_service_spec.rb
+++ spec/services/service/contributions/contribution_fee_calculator_service_spec.rb
@@ -3,5 +3,40 @@
 require 'rails_helper'
 
 describe Service::Contributions::ContributionFeeCalculatorService do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:service) do
+    described_class.new(payer_contribution_balance:, fee_to_be_paid:,
+                        initial_contributions_balance:)
+  end
+
+  let(:payer_contribution_balance) { create(:contribution_balance, fees_balance_cents: 5000) }
+  let(:initial_contributions_balance) { 10_000 }
+  let(:fee_to_be_paid) { 100 }
+
+  before do
+    create(:ribon_config, minimum_contribution_chargeable_fee_cents: 10)
+  end
+
+  describe '#calculate_proportional_fee' do
+    context 'when the fee to be paid is higher then the minimum' do
+      it 'calculates the fee based on the total amount and the payer contribution balance' do
+        expect(service.calculate_proportional_fee).to eq(50)
+      end
+    end
+
+    context 'when the fee to be paid is less then the minimum' do
+      let(:fee_to_be_paid) { 10 }
+
+      it 'returns the minimum fee' do
+        expect(service.calculate_proportional_fee).to eq(10)
+      end
+    end
+  end
+
+  describe '#calculate_increased_value_for' do
+    let(:contribution) { create(:contribution, person_payment: create(:person_payment, crypto_value_cents: 1000)) }
+
+    it 'returns the percentage of fee paid relative to the contribution value' do
+      expect(service.calculate_increased_value_for(contribution:)).to eq(500)
+    end
+  end
 end

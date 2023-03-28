304def326e068f319390716f71a994b5856ad3ec
mendesmcmg <claraplanejamento@gmail.com>
2023-03-28 17:52:15 -0300

    feat: adds new big donor 'create' method

--- app/controllers/api/v1/big_donors_controller.rb
+++ app/controllers/api/v1/big_donors_controller.rb
@@ -6,6 +6,22 @@ module Api
 
         render json: BigDonorBlueprint.render(@big_donors)
       end
+
+      def create
+        command = BigDonors::CreateBigDonor.call(big_donor_params)
+
+        if command.success?
+          render json: BigDonorBlueprint.render(command.result), status: :created
+        else
+          render_errors(command.errors)
+        end
+      end
+
+      private
+
+      def big_donor_params
+        params.permit(:name, :email)
+      end
     end
   end
 end

0653dc79ac0749de9a7adba4aef75ccdb15a94a7
Nicholas Marques <devnicknish@gmail.com>
2023-03-28 10:31:26 -0300

    feat: add DonationContribution creation to TicketLabelingService

--- app/services/service/contributions/ticket_labeling_service.rb
+++ app/services/service/contributions/ticket_labeling_service.rb
@@ -8,7 +8,25 @@ module Service
         @initial_tickets_balance_cents = ContributionBalance.sum(:tickets_balance_cents)
       end
 
-      def spread_tickets_to_payers; end
+      def label_donation
+        contribution_to_label = next_contribution_to_label_ticket
+
+        DonationContribution.create!(contribution: next_contribution_to_label_ticket, donation:)
+        update_contribution_balance(contribution_balance: contribution_to_label.contribution_balance)
+      rescue StandardError => e
+        Reporter.log(error: e)
+      end
+
+      def update_contribution_balance(contribution_balance:)
+        contribution_balance.tickets_balance_cents -= donation.value
+        contribution_balance.save!
+      end
+
+      def next_contribution_to_label_ticket
+        return contributions_with_less_than_10_percent.last if contributions_with_less_than_10_percent.any?
+
+        ordered_contributions.to_a.last
+      end
 
       def last_contribution_payer_type
         DonationContribution.last.contribution&.person_payment&.payer_type
@@ -16,17 +34,21 @@ module Service
 
       def contributions_by_payer_type
         if last_contribution_payer_type == 'BigDonor'
-          return Contribution.joins(:contribution_balance, :person_payment)
-                             .where('person_payments.payer_type IN (?, ?)', 'Customer', 'CryptoUser')
+          promoter_contributions = Contribution
+                                   .joins(:contribution_balance, :person_payment)
+                                   .where('person_payments.payer_type IN (?, ?)', 'Customer', 'CryptoUser')
+                                   .where('contribution_balances.tickets_balance_cents > 0')
+
+          return promoter_contributions if promoter_contributions.any?
         end
 
         Contribution.joins(:contribution_balance, :person_payment)
                     .where(person_payments: { payer_type: 'BigDonor' })
+                    .where('contribution_balances.tickets_balance_cents > 0')
       end
 
       def contributions_with_less_than_10_percent
         contributions_by_payer_type
-          .where('contribution_balances.tickets_balance_cents > 0')
           .where('contribution_balances.tickets_balance_cents <= 0.1 * person_payments.crypto_value_cents')
       end
 

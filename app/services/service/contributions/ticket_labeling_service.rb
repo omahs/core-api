7ce40141e058d8af05b97dd5f406034f6646fba8
Nicholas Marques <devnicknish@gmail.com>
2023-03-28 09:58:53 -0300

    feat: add contribution_by_payer_type to TicketLabelingService

--- app/services/service/contributions/ticket_labeling_service.rb
+++ app/services/service/contributions/ticket_labeling_service.rb
@@ -10,25 +10,28 @@ module Service
 
       def spread_tickets_to_payers; end
 
-      private
-
-      def calculate_proportional_tickets_distribution; end
-
       def last_contribution_payer_type
         DonationContribution.last.contribution&.person_payment&.payer_type
       end
 
-      def next_to_pay
-        ordered_contributions.last
+      def contributions_by_payer_type
+        if last_contribution_payer_type == 'BigDonor'
+          return Contribution.joins(:contribution_balance, :person_payment)
+                             .where('person_payments.payer_type IN (?, ?)', 'Customer', 'CryptoUser')
+        end
+
+        Contribution.joins(:contribution_balance, :person_payment)
+                    .where(person_payments: { payer_type: 'BigDonor' })
       end
 
-      def contributions_by_payer_type
-        ordered_contributions
-          .group_by { |contribution| contribution.person_payment.payer_type }
+      def contributions_with_less_than_10_percent
+        contributions_by_payer_type
+          .where('contribution_balances.tickets_balance_cents > 0')
+          .where('contribution_balances.tickets_balance_cents <= 0.1 * person_payments.crypto_value_cents')
       end
 
       def ordered_contributions
-        Contribution.joins(
+        contributions_by_payer_type.joins(
           "LEFT OUTER JOIN (
             SELECT MAX(created_at) AS last_donation_created_at, contribution_id
             FROM donation_contributions

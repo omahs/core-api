8ad01545e611faa9e4874a446c036d68bbe699de
letbueno <leticia.bueno.martins@gmail.com>
2023-03-30 09:19:32 -0300

    feat: add language on articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_151405) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_182025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_151405) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "link"
+    t.string "language"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 

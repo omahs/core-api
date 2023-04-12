a83779c0c89df465dc4a0bad8f624dbcb3885930
Yan Galli <yangalli@hotmail.com>
2023-04-12 14:29:04 -0300

    fix: update logic to calculate proportional_fee

--- spec/services/service/contributions/contribution_fee_calculator_service_spec.rb
+++ spec/services/service/contributions/contribution_fee_calculator_service_spec.rb
@@ -16,10 +16,10 @@ describe Service::Contributions::ContributionFeeCalculatorService do
     create(:ribon_config, minimum_contribution_chargeable_fee_cents: 10)
   end
 
-  describe '#calculate_proportional_fee' do
+  describe '#proportional_fee' do
     context 'when the fee to be paid is higher then the minimum' do
       it 'calculates the fee based on the total amount and the payer contribution balance' do
-        expect(service.calculate_proportional_fee).to eq(50)
+        expect(service.proportional_fee).to eq(50)
       end
     end
 
@@ -27,16 +27,16 @@ describe Service::Contributions::ContributionFeeCalculatorService do
       let(:fee_to_be_paid) { 10 }
 
       it 'returns the minimum fee' do
-        expect(service.calculate_proportional_fee).to eq(10)
+        expect(service.proportional_fee).to eq(10)
       end
     end
   end
 
-  describe '#calculate_increased_value_for' do
+  describe '#increased_value_for' do
     let(:contribution) { create(:contribution, person_payment: create(:person_payment, usd_value_cents: 1000)) }
 
     it 'returns the percentage of fee paid relative to the contribution value' do
-      expect(service.calculate_increased_value_for(contribution:)).to eq(500)
+      expect(service.increased_value_for(contribution:)).to eq(500)
     end
   end
 end

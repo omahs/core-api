44c4247bd1bd813ae3c63f78a00760b618a93445
Nicholas Marques <devnicknish@gmail.com>
2023-04-13 14:00:28 -0300

    refactor: update fees_labeling_service methods

--- app/services/service/contributions/fees_labeling_service.rb
+++ app/services/service/contributions/fees_labeling_service.rb
@@ -12,9 +12,9 @@ module Service
 
       def spread_fee_to_payers
         deal_with_fees_balances_empty
+        accumulated_fees_result = fee_generated_by_new_contribution.ceil
 
-        ordered_feeable_contribution_balances.reduce(initial_fee_generated_by_new_contribution.ceil) do
-        |accumulated_fees_result, contribution_balance|
+        feeable_contribution_balances.each do |contribution_balance|
           if last_payer?(accumulated_fees_result:, contribution_balance:)
             charge_remaining_fee_from_last_contribution_balance(accumulated_fees_result:, contribution_balance:)
             break 0
@@ -22,47 +22,52 @@ module Service
 
           fee_cents = calculate_fee_for(contribution_balance:)
           contribution_increased_amount_cents = calculate_increased_value_for(contribution_balance:)
-          create_contribution_fee(contribution_balance:, fee_cents:,
-                                  payer_contribution_increased_amount_cents: contribution_increased_amount_cents)
-          update_contribution_balance(contribution_balance:, fee_cents:, contribution_increased_amount_cents:)
+          handle_fee_creation_for(contribution_balance:, fee_cents:, contribution_increased_amount_cents:)
 
-          accumulated_fees_result - fee_cents
+          accumulated_fees_result -= fee_cents
         end
       end
 
       private
 
+      def handle_fee_creation_for(contribution_balance:, fee_cents:, contribution_increased_amount_cents:)
+        create_contribution_fee(
+          contribution_balance:, fee_cents:,
+          payer_contribution_increased_amount_cents: contribution_increased_amount_cents
+        )
+        update_contribution_balance(contribution_balance:, fee_cents:, contribution_increased_amount_cents:)
+      end
+
       def last_payer?(accumulated_fees_result:, contribution_balance:)
-        accumulated_fees_result < minimum_fee || contribution_balance == ordered_feeable_contribution_balances.last
+        accumulated_fees_result < minimum_fee || contribution_balance == feeable_contribution_balances.last
       end
 
       def minimum_fee
         RibonConfig.minimum_contribution_chargeable_fee_cents
       end
 
-      def initial_fee_generated_by_new_contribution
+      def fee_generated_by_new_contribution
         contribution.generated_fee_cents
       end
 
-      def ordered_feeable_contribution_balances
-        @ordered_feeable_contribution_balances ||= ContributionQueries
-                                                   .new(contribution:)
-                                                   .ordered_feeable_contribution_balances
+      def feeable_contribution_balances
+        @feeable_contribution_balances ||= ContributionQueries.new(contribution:)
+                                                              .ordered_feeable_contribution_balances
+      end
+
+      def contribution_fee_calculator_service(contribution_balance:)
+        payer_balance = contribution_balance.fees_balance_cents
+        fee_to_be_paid = fee_generated_by_new_contribution
+
+        ContributionFeeCalculatorService.new(payer_balance:, fee_to_be_paid:, initial_contributions_balance:)
       end
 
       def calculate_fee_for(contribution_balance:)
-        ContributionFeeCalculatorService
-          .new(payer_balance: contribution_balance.fees_balance_cents,
-               fee_to_be_paid: initial_fee_generated_by_new_contribution,
-               initial_contributions_balance:).proportional_fee
+        contribution_fee_calculator_service(contribution_balance:).proportional_fee
       end
 
       def calculate_increased_value_for(contribution_balance:)
-        ContributionFeeCalculatorService
-          .new(payer_balance: contribution_balance.fees_balance_cents,
-               fee_to_be_paid: initial_fee_generated_by_new_contribution,
-               initial_contributions_balance:)
-          .increased_value_for(contribution:)
+        contribution_fee_calculator_service(contribution_balance:).increased_value_for(contribution:)
       end
 
       def update_contribution_balance(contribution_balance:, fee_cents:, contribution_increased_amount_cents:)
@@ -95,10 +100,10 @@ module Service
       end
 
       def deal_with_fees_balances_empty
-        return unless ordered_feeable_contribution_balances.empty?
+        return unless feeable_contribution_balances.empty?
 
         HandleRemainingContributionFee
-          .new(contribution:, remaining_fee: initial_fee_generated_by_new_contribution).spread_remaining_fee
+          .new(contribution:, remaining_fee: fee_generated_by_new_contribution).spread_remaining_fee
       end
     end
   end

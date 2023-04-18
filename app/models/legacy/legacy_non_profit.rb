c9263a32430ab2051bcf9ae05d3d7806403f268e
Yan Galli <yangalli@hotmail.com>
2023-04-18 10:14:07 -0300

    feat: add LegacyNonProfit table to migrated data from web2

---
+++ app/models/legacy/legacy_non_profit.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: legacy_non_profits
+#
+#  id                 :bigint           not null, primary key
+#  cost_of_one_impact :integer
+#  impact_description :string
+#  logo_url           :string
+#  name               :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  current_id         :integer
+#  legacy_id          :integer
+#
+class LegacyNonProfit < ApplicationRecord
+  validates :name, :logo_url, :cost_of_one_impact, :impact_description, :legacy_id, :current_id, presence: true
+end
---
+++ spec/models/legacy/legacy_non_profit_spec.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: legacy_non_profits
+#
+#  id                 :bigint           not null, primary key
+#  cost_of_one_impact :integer
+#  impact_description :string
+#  logo_url           :string
+#  name               :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  current_id         :integer
+#  legacy_id          :integer
+#
+require 'rails_helper'
+
+RSpec.describe LegacyNonProfit, type: :model do
+  describe '.validations' do
+    subject { build(:legacy_non_profit) }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:logo_url) }
+    it { is_expected.to validate_presence_of(:cost_of_one_impact) }
+    it { is_expected.to validate_presence_of(:impact_description) }
+    it { is_expected.to validate_presence_of(:legacy_id) }
+    it { is_expected.to validate_presence_of(:current_id) }
+  end
+end

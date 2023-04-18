d6854c57971ea7a37613c3dcc94fc8b04c1d129f
mendesmcmg <claraplanejamento@gmail.com>
2023-04-18 12:50:20 -0300

    feat: adds legacy_id on users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_125247) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_154736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -516,6 +516,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_125247) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "language", default: 0
+    t.integer "legacy_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

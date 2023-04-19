06c40cb2e9789514757c8ac009978a4c3cc23754
Thais Correa <thais@ribon.io>
2023-04-19 11:10:53 -0300

    feat: big donation payment by manager

--- db/schema.rb
+++ db/schema.rb
@@ -446,6 +446,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_183002) do
     t.index ["non_profit_id"], name: "index_stories_on_non_profit_id"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "actions"
+    t.text "rules"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tokens", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -504,7 +512,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_183002) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "language", default: 0
+    t.integer "language"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

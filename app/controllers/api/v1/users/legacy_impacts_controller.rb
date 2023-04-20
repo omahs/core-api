78c3c442f6c68388d328ca5e3e0bc6b25c6f1b9e
Nicholas Marques <devnicknish@gmail.com>
2023-04-20 11:01:45 -0300

    feat: add legacy impacts controller

---
+++ app/controllers/api/v1/users/legacy_impacts_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    module Users
+      class LegacyImpactsController < ApplicationController
+        def index
+          render json: user.legacy_user_impacts
+        end
+
+        private
+
+        def user
+          @user ||= User.find params[:user_id]
+        end
+      end
+    end
+  end
+end

cf1b07e7fdd7bc0612891238c4f6757660542685
mendesmcmg <claraplanejamento@gmail.com>
2023-04-24 15:27:13 -0300

    refactor: changes method name

--- app/controllers/api/v1/manager/pools_controller.rb
+++ app/controllers/api/v1/manager/pools_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     module Manager
       class PoolsController < ApplicationController
-        def index_manager
+        def index
           @pools = Pool.all
 
           render json: PoolBlueprint.render(@pools, view: :manager)

33a70dfa7a13ca55c5959b463ff1bb478eb04f37
mendesmcmg <claraplanejamento@gmail.com>
2023-04-27 12:59:23 -0300

    feat: adds deleted at field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_26_173202) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_153720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -533,6 +533,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_26_173202) do
     t.datetime "updated_at", null: false
     t.integer "language", default: 0
     t.integer "legacy_id"
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

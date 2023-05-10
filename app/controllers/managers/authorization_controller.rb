77a1096928216b80a2c485561d2151d3a154c4be
mendesmcmg <claraplanejamento@gmail.com>
2023-05-10 08:59:56 -0300

    fix: set correct namespace for authorization contr

--- app/controllers/managers/authorization_controller.rb
+++ app/controllers/managers/authorization_controller.rb
@@ -1,31 +1,29 @@
 module Managers
-  module V1
-    class AuthorizationController < ApplicationController
-      def google_authorization
-        command = ::Manager::SetUserManagerTokens.call(id_token: params[:data]['id_token'])
+  class AuthorizationController < ApplicationController
+    def google_authorization
+      command = ::Manager::SetUserManagerTokens.call(id_token: params[:data]['id_token'])
 
-        if command.success?
-          create_headers(command.result)
+      if command.success?
+        create_headers(command.result)
 
-          render json: { message: I18n.t('manager.login_success') }, status: :created
-        else
-          render_errors(command.errors)
-        end
+        render json: { message: I18n.t('manager.login_success') }, status: :created
+      else
+        render_errors(command.errors)
       end
+    end
 
-      private
+    private
 
-      def create_headers(tokens)
-        set_header('access-token', tokens['access-token'])
-        set_header('client', tokens['client'])
-        set_header('expiry', tokens['expiry'])
-        set_header('uid', tokens['uid'])
-        set_header('token-type', tokens['token-type'])
-      end
+    def create_headers(tokens)
+      set_header('access-token', tokens['access-token'])
+      set_header('client', tokens['client'])
+      set_header('expiry', tokens['expiry'])
+      set_header('uid', tokens['uid'])
+      set_header('token-type', tokens['token-type'])
+    end
 
-      def set_header(name, value)
-        headers[name] = value.to_s
-      end
+    def set_header(name, value)
+      headers[name] = value.to_s
     end
   end
 end

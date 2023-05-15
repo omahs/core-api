2c4347b4ae54f5c924c1a70e75e169f899751c1d
Nicholas Marques <devnicknish@gmail.com>
2023-05-15 15:22:19 -0300

    test: add legacy_user associations test

--- spec/models/legacy_user_spec.rb
+++ spec/models/legacy_user_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe LegacyUser, type: :model do
   describe '.validations' do
     subject { build(:legacy_user) }
 
+    it { is_expected.to belong_to(:user).optional }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:legacy_id) }
     it { is_expected.to have_many(:legacy_user_impacts) }
